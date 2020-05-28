2f691a63d06625a4b41f6abd99114c2f4d852cc4
Morten Høfft <mhoefft@gbif.org>
2020-05-28 16:15:25 +0200

    add occurrence type details to graphql

---
+++ packages/es-api/src/requestAdapter/util/suggestGraphqlType.js
@@ -0,0 +1,126 @@
+/**
+ * Given an ES endpoint, then try to extract a sensible graphql type given its mappings.
+ * It is not intended to be used blindly, but as a starting point.
+ */
+const axios = require('axios');
+const _ = require('lodash');
+
+async function suggestGqlTypeFromAlias({endpoint, alias, type}) {
+  const aliasResponse = await axios.get(`${endpoint}/${alias}/_alias`);
+  const index = Object.keys(aliasResponse.data)[0];
+  return suggestGqlTypeFromIndex({endpoint, index, type});
+}
+
+async function suggestGqlTypeFromIndex({endpoint, index, type}) {
+  const mappingResponse = await axios.get(`${endpoint}/${index}/_mapping`);
+  const mapping = mappingResponse.data[index].mappings[type];
+  const config = mapping2type(mapping);
+
+  printType('Occurrence', config);
+
+  return config;
+}
+
+function typeCase(s) {
+  let str = _.camelCase(s);
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function printType(name, fields) {
+  let backlog = [];
+  let str = `type ${typeCase(name)} {`;
+  fields.forEach(x => {
+    if (x.type !== 'NEW_TYPE')
+      str += `\n  ${x.fieldName}: ${x.type}`;
+    else {
+      backlog.push({name: typeCase(x.name), fields: x.fields})
+      str += `\n  ${x.name}: ${typeCase(x.name)}`;
+    }
+  });
+  str += `\n}\n`;
+  console.log(str);
+
+  backlog.forEach(type => {
+    printType(type.name, type.fields);
+  });
+}
+
+function mapping2type(mapping, prefix) {
+  //process them
+  const fieldConfigs = Object.keys(mapping.properties).map(field => {
+    const fieldConfig = mapping.properties[field];
+    
+    switch (fieldConfig.type) {
+      case 'text': return {
+        fieldName: field,
+        type: 'String'
+      }
+      case 'date': return {
+        fieldName: field,
+        type: 'Date'
+      }
+      case 'integer': return {
+        fieldName: field,
+        type: 'Int'
+      }
+      case 'double': return {
+        fieldName: field,
+        type: 'Float'
+      }
+      case 'short': return {
+        fieldName: field,
+        type: 'Float'
+      }
+      case 'float': return {
+        fieldName: field,
+        type: 'Float'
+      }
+      case 'long': return {
+        fieldName: field,
+        type: 'Integer'
+      }
+      case 'keyword': return {
+        fieldName: field,
+        type: 'String'
+      }
+      case 'boolean': return {
+        fieldName: field,
+        type: 'Boolean'
+      }
+      case 'geo_shape': return {
+        fieldName: field,
+        type: 'String'
+      }
+      case 'nested': return {
+        fieldName: field,
+        type: 'JSON'
+      }
+      default: {
+        if (typeof fieldConfig.type === 'undefined' && _.isObject(fieldConfig.properties)) {
+          // it is a deep property and should be a seperate type
+          return {
+            type: 'NEW_TYPE',
+            name: field,
+            fields: mapping2type(fieldConfig, field),
+          }
+        } else {
+          return {
+            field,
+            discarded: true
+          }
+        }
+      }
+    }
+  });
+
+  //mutate array by deleting discarded
+  const options = fieldConfigs.filter(x => !x.discarded);
+
+  return options;
+}
+
+module.exports = {
+  suggestGqlTypeFromAlias,
+  suggestGqlTypeFromIndex,
+  mapping2type
+}
--- packages/graphql-api/src/resources/occurrence/occurrence.type.js
+++ packages/graphql-api/src/resources/occurrence/occurrence.type.js
@@ -227,6 +227,179 @@ const typeDef = gql`
 
   type Occurrence {
     gbifId: ID!
+    
+    basisOfRecord: String
+    catalogNumber: String
+    collectionCode: String
+    collectionKey: String
+    continent: String
+    coordinatePrecision: Float
+    coordinateUncertaintyInMeters: Float
+    country: String
+    countryCode: String
+    crawlId: Int
+    created: DateTime
+    datasetKey: String
+    datasetPublishingCountry: String
+    datasetTitle: String
+    dateIdentified: DateTime
+    day: Float
+    decimalLatitude: Float
+    decimalLongitude: Float
+    depth: Float
+    depthAccuracy: Float
+    elevation: Float
+    elevationAccuracy: Float
+    endDayOfYear: Float
+    endorsingNodeKey: String
+    establishmentMeans: String
+    eventDateSingle: DateTime
+    eventId: String
+    gbifClassification: GbifClassification
+    hasCoordinate: Boolean
+    hasGeospatialIssue: Boolean
+    id: String
+    individualCount: Int
+    installationKey: String
+    institutionCode: String
+    institutionKey: String
+    issues: String
+    lastCrawled: DateTime
+    license: String
+    lifeStage: String
+    locality: String
+    maximumDepthInMeters: Float
+    maximumDistanceAboveSurfaceInMeters: Float
+    maximumElevationInMeters: Float
+    measurementOrFactItems: JSON
+    mediaLicenses: String
+    mediaTypes: String
+    minimumDepthInMeters: Float
+    minimumDistanceAboveSurfaceInMeters: Float
+    minimumElevationInMeters: Float
+    modified: DateTime
+    month: Float
+    multimediaItems: JSON
+    networkKeys: String
+    notIssues: String
+    occurrenceId: String
+    organismId: String
+    organismQuantity: Float
+    organismQuantityType: String
+    parentEventId: String
+    programmeAcronym: String
+    projectId: String
+    protocol: String
+    publisherTitle: String
+    publishingCountry: String
+    publishingOrganizationKey: String
+    recordNumber: String
+    recordedBy: String
+    references: String
+    relativeOrganismQuantity: Float
+    repatriated: Boolean
+    sampleSizeUnit: String
+    sampleSizeValue: Float
+    samplingProtocol: String
+    scoordinates: String
+    sex: String
+    startDayOfYear: Float
+    stateProvince: String
+    typeStatus: String
+    typifiedName: String
+    waterBody: String
+    year: Float
+  }
+
+  type GbifClassification {
+    acceptedUsage: AcceptedUsage
+    class: String
+    classKey: Int
+    classification: Classification
+    classificationPath: String
+    diagnostics: Diagnostics
+    family: String
+    familyKey: Int
+    genus: String
+    genusKey: Int
+    kingdom: String
+    kingdomKey: Int
+    order: String
+    orderKey: Int
+    phylum: String
+    phylumKey: Int
+    species: String
+    speciesKey: Int
+    synonym: Boolean
+    taxonID: String
+    taxonKey: Int
+    usage: Usage
+    usageParsedName: UsageParsedName
+    verbatimScientificName: String
+  }
+
+  type AcceptedUsage {
+    key: Int
+    name: String
+    rank: String
+  }
+
+  type Classification {
+    key: Int
+    name: String
+    rank: String
+    synonym: Boolean
+  }
+
+  type Diagnostics {
+    matchType: String
+    note: String
+    status: String
+  }
+
+  type Usage {
+    key: Int
+    name: String
+    rank: String
+  }
+
+  type UsageParsedName {
+    abbreviated: Boolean
+    autonym: Boolean
+    basionymAuthorship: BasionymAuthorship
+    binomial: Boolean
+    candidatus: Boolean
+    code: String
+    combinationAuthorship: CombinationAuthorship
+    doubtful: Boolean
+    genericName: String
+    genus: String
+    incomplete: Boolean
+    indetermined: Boolean
+    infraspecificEpithet: String
+    notho: String
+    rank: String
+    specificEpithet: String
+    state: String
+    terminalEpithet: String
+    trinomial: Boolean
+    type: String
+    uninomial: String
+    unparsed: String
+  }
+
+  type BasionymAuthorship {
+    authors: String
+    empty: Boolean
+    exAuthors: String
+    year: String
+  }
+
+  type CombinationAuthorship {
+    authors: String
+    empty: Boolean
+    exAuthors: String
+    year: String
   }
 `;
 
