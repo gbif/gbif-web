28e72f988ab2e8d9473424a702fb2f2872000c4b
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-01-27 09:14:16 +0100

    Improve validation of ENUM schema on startup

--- packages/graphql-api/src/enums/index.js
+++ packages/graphql-api/src/enums/index.js
@@ -4,28 +4,31 @@ const fs = require('fs').promises;
 const got = require('got');
 const _ = require('lodash');
 const config = require('../config');
+const { gql } = require("apollo-server");
 const API_V1 = config.apiv1;
 
-async function loadEnums(fileName) {
+async function loadEnums() {
   const types = await getEnumData('enumeration/basic')
   const enums = await Promise.all(
     types.map(type => getEnumData(`enumeration/basic/${type}`))
   );
   const enumMap = _.zipObject(types, enums);
-  return fs.writeFile(`${__dirname}/${fileName}`, JSON.stringify(enumMap, null, 2));
+  return enumMap;
+  // return fs.writeFile(`${__dirname}/${fileName}`, JSON.stringify(enumMap, null, 2));
 }
 
+/* 
 async function getLatestInterpretationRemark(){
   // Discretely write latest interpretationRemark
   try {
     const interpretationRemarkLatest = await getEnumData('enumeration/interpretationRemark')
-    await fs.writeFile(`${__dirname}/interpretationRemark_latest.json`, JSON.stringify(interpretationRemarkLatest, null, 2));
+    await fs.writeFile(`${__dirname}/interpretationRemark.json`, JSON.stringify(interpretationRemarkLatest, null, 2));
    } catch(error){
      console.log("Failed to fetch latest interpretationRemark from API:")
      console.log(error)
    } 
 }
-
+ */
 
 async function getEnumData(url) {
   const res = await got(url, {
@@ -38,46 +41,85 @@ async function getEnumData(url) {
   return res.body;
 }
 
-async function writeLatestEnums() {
+async function writeEnums(enumMap) {
   try {
-    await loadEnums('enums_latest.json');
-    console.log("Latest version Enums written to 'enums_latest.json'")
-    await getLatestInterpretationRemark();
-    console.log("Latest version of interpretationRemark written to 'interpretationRemark_latest.json'")
+    const prevVersionEnums = JSON.stringify(require('./enums.json'), null, 2);
+    const currentVersionEnums = JSON.stringify(enumMap, null, 2);
+    console.log(currentVersionEnums)
+    if(prevVersionEnums !== currentVersionEnums){
+      console.log("New version of ENUMS detected and validated, writing to enums.json")
+      return fs.writeFile(`${__dirname}/enums.json`, currentVersionEnums);
+    } else {
+      console.log("No changes in ENUMS")
+      return Promise.resolve();
+    }    
   } catch(error){
-    console.log("Failed to fetch Enums from API: ");
+    console.log("Failed to write Enums.");
     console.log(error)
   }
 }
 
-async function getEnumTypeDefs() {
-  //Load enums from file - if the file is not there, get map of enums from API first
-  let enums;
+
+const schemaIsValid = (enumsSchema) => {
   try {
-    enums = require('./enums.json');
-    writeLatestEnums()
-  } catch(err){
-    try {
-      await loadEnums('enums.json');
-      console.log("Enums written to file from API")
-      return getEnumTypeDefs()
-    } catch(error){
-      console.log("Failed to fetch Enums from API: ");
-      console.log(error)
-    }
-  }  
-  //map enums to schema definitions
+    gql`
+      ${enumsSchema}
+
+      type Query {
+        """
+        _empty is nonsense, and only here as we are not allowed to extend an empty type.
+        """
+        _empty: String
+      }
+    `;
+    return {valid: true, error: null};
+  } catch (err) {
+    return {valid: false, error: err};
+  }
+};
+
+const getSchema = enums => {
   const schemas = Object.keys(enums).map(enumType => {
     const list = enums[enumType].reduce( (accumulator, currentValue) => accumulator += currentValue + '\n', '');
+    // enumType.replace(/\./g, '_')
     return `
       enum ${enumType.replace(/\./g, '_')} {
       ` + 
         list + `}
       `
   });
-  const schema = schemas.reduce( (acc, curr) => acc + curr, '');
-  return schema;
+  return schemas.reduce( (acc, curr) => acc + curr, '');
 }
+
+async function getEnumTypeDefs() {
+  // Should we fetch interpretationRemark also? 
+  // getLatestInterpretationRemark();
+  // Load enums from file - if the file is not there, get map of enums from API first
+  let enums;
+  try {
+    enums = await loadEnums() //require('./enums.json');
+     //writeLatestEnums()
+  } catch(err){
+    enums = require('./enums.json');
+    console.log("Failed to fetch Enums from API. Using fallback enums.json");
+    console.log(err)
+  }  
+  let schema = getSchema(enums)
+  const validationResult = schemaIsValid(schema);
+  if(validationResult.valid){
+    // GraphQL is happy about the enum schema - write it to enums.json and return
+    writeEnums(enums) 
+    return schema;
+  } else {
+    // GraphQL is sad about the enum schema - fetch the backup from last successful attempt
+    console.log("ENUM schema validation failed:")
+    console.log(validationResult.error)
+    console.log("Using fallback enums.json.")
+    enums = require('./enums.json');
+    return getSchema(enums);
+  }
+}
+
 
 module.exports = {
   getEnumTypeDefs
