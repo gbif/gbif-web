e85973e5613c7c1781bd71845663642fa80678cb
Morten Høfft <mhoefft@gbif.org>
2021-03-19 12:28:05 +0100

    chore: fix broken import

--- packages/react-components/src/components/index.js
+++ packages/react-components/src/components/index.js
@@ -22,7 +22,7 @@ export { DetailsDrawer } from './DetailsDrawer/DetailsDrawer';
 export { MajorRanks } from './MajorRanks/MajorRanks';
 export { IconFeatures } from './IconFeatures/IconFeatures';
 export { HyperText } from './HyperText/HyperText';
-export { Doi } from './Doi/Doi'
+export { default as Doi } from './Doi/Doi'
 export { Progress } from './Progress/Progress';
 export { Classification } from './Classification/Classification';
 export { Tag, Tags } from './Tag/Tag';
