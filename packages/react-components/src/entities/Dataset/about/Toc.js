2d70de735cd1e18d50fd61f74e191ed4445fda1a
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-08-23 16:08:27 +0200

    offset prop for TOC for fixed headers

--- packages/react-components/src/entities/Dataset/about/Toc.js
+++ packages/react-components/src/entities/Dataset/about/Toc.js
@@ -5,7 +5,7 @@ import * as css from "./styles";
 import { useLocation, useHistory } from "react-router-dom";
 import _ from "lodash";
 
-export function Toc({ refs, ...props }) {
+export function Toc({ refs, offsetTop = 0, ...props }) {
   const theme = useContext(ThemeContext);
   const location = useLocation();
   const history = useHistory();
@@ -19,12 +19,12 @@ export function Toc({ refs, ...props }) {
     setSections(Object.keys(refs));
     const handleScroll = (e) => {
       const top = window.pageYOffset || document.documentElement.scrollTop;
-      if (top > 200) {
+      if (top > 200 + offsetTop) {
         Object.keys(refs).forEach((hash) => {
           const elm = refs[hash];
           if (elm) {
             const rect = elm.getBoundingClientRect();
-            if (rect.top > 0 && rect.top < 150) {
+            if (rect.top > offsetTop && rect.top < 150 + offsetTop) {
               history.push({ ...location, hash });
               setActiveSection(hash);
             }
@@ -41,7 +41,6 @@ export function Toc({ refs, ...props }) {
 
  
   const scrollToSection = (elmn) => {
-   // const elmn = document.getElementById(sect);
     if (elmn) {
       elmn.scrollIntoView();
     }
