741c1a5d577cd538f4f1e5ee67829bc40df1085a
Morten Høfft <mhoefft@gbif.org>
2021-08-23 13:55:57 +0200

    feat: set default routing for detail pages to gbif.org

--- packages/react-components/src/dataManagement/RouteContext.js
+++ packages/react-components/src/dataManagement/RouteContext.js
@@ -1,11 +1,14 @@
 import React from 'react';
+const gbifOrg = 'https://www.gbif.org';
+
 export default React.createContext({
   occurrenceSearch: {
     url: ({queryString}) => `/occurrence/search${queryString ? `?${queryString}` : ''}`,
     route: '/occurrence/search'
   },
   collectionKey: {
-    url: ({key}) => `/collection/${key}`,
+    // url: ({key}) => `/collection/${key}`,
+    url: ({key}) => `${gbifOrg}/grscicoll/collection/${key}`,
     route: '/collection/:key'
   },
   collectionSearch: {
@@ -16,7 +19,8 @@ export default React.createContext({
   },
 
   institutionKey: {
-    url: ({key}) => `/institution/${key}`,
+    // url: ({key}) => `/institution/${key}`,
+    url: ({key}) => `${gbifOrg}/grscicoll/institution/${key}`,
     route: '/institution/:key'
   },
   institutionSearch: {
@@ -24,7 +28,8 @@ export default React.createContext({
   },
 
   datasetKey: {
-    url: ({key}) => `/dataset/${key}`,
+    // url: ({key}) => `/dataset/${key}`,
+    url: ({key}) => `${gbifOrg}/dataset/${key}`,
     route: '/dataset/:key'
   },
   datasetSearch: {
@@ -32,7 +37,8 @@ export default React.createContext({
   },
 
   publisherKey: {
-    url: ({key}) => `/publisher/${key}`,
+    // url: ({key}) => `/publisher/${key}`,
+    url: ({key}) => `${gbifOrg}/publisher/${key}`,
     route: '/publisher/:key'
   },
   publisherSearch: {
