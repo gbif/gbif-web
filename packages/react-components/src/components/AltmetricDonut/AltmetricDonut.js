5acea2811c1cdcc605717b1454d06fc904795b41
Morten Høfft <mhoefft@gbif.org>
2021-08-23 15:14:57 +0200

    feat: altetrics should not do anything when doi issn't a string

--- packages/react-components/src/components/AltmetricDonut/AltmetricDonut.js
+++ packages/react-components/src/components/AltmetricDonut/AltmetricDonut.js
@@ -4,6 +4,8 @@ import axios from '../../dataManagement/api/axios';
 
 export function AltmetricDonut({doi, ...props}) {
   const [donut, setDonut] = useState();
+  if (typeof doi !== 'string') return;
+  
   useEffect(() => {
     const response = axios.get(`https://api.altmetric.com/v1/doi/${doi}`);
     response
