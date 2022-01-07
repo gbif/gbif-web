d7a6bd8aa0c2fd09f6dd1899cfe3faa6e0af40d4
Morten Høfft <mhoefft@gbif.org>
2022-01-07 13:03:11 +0100

    chore: remove log statement

--- packages/react-components/src/search/OccurrenceSearch/Standalone.js
+++ packages/react-components/src/search/OccurrenceSearch/Standalone.js
@@ -5,7 +5,6 @@ import OccurrenceSearch from "./OccurrenceSearch";
 
 function Standalone(props) {
   const path = window.location.pathname;
-  console.log(props);
   return <StandaloneWrapper {...props}>
     <Switch>
       <Route
