43852c3873351857e821892abfb137f50f0af4fe
Morten Høfft <mhoefft@gbif.org>
2022-01-10 12:58:50 +0100

    close https://github.com/gbif/hosted-portals/issues/201

--- packages/react-components/src/entities/OccurrenceSidebar/details/Header.js
+++ packages/react-components/src/entities/OccurrenceSidebar/details/Header.js
@@ -29,10 +29,13 @@ export function Header({
         <Eyebrow 
           style={{fontSize: '80%'}}
           prefix={<FormattedMessage id="occurrenceDetails.occurrence" />} 
-          suffix={<FormattedDate value={data?.occurrence?.eventDate}
-          year="numeric"
-          month="long"
-          day="2-digit" />} />
+          suffix={data?.occurrence?.eventDate ? 
+            <FormattedDate value={data?.occurrence?.eventDate}
+              year="numeric"
+              month="long"
+              day="2-digit" /> : 
+            <FormattedMessage id="phrases.unknownDate"/>} 
+            />
         <h3 dangerouslySetInnerHTML={{ __html: data?.occurrence?.gbifClassification?.usage?.formattedName }}></h3>
         {/* <div style={{color: 'orange', marginTop: 4}}>Published as: Polycauliona polycarpa hoffman</div> */}
         {/* <div style={{fontSize: 13}}><MajorRanks taxon={data?.occurrence?.gbifClassification} rank={data?.occurrence?.gbifClassification?.usage?.rank}/></div> */}
