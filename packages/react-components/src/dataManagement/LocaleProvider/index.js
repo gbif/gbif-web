90afb99a649d4a0991c9232f3cd7ed68731182f2
Morten Høfft <mhoefft@gbif.org>
2022-03-03 14:41:26 +0100

    feat: allow overwriting messages

--- packages/react-components/src/dataManagement/LocaleProvider/index.js
+++ packages/react-components/src/dataManagement/LocaleProvider/index.js
@@ -3,9 +3,9 @@ import { IntlProvider } from "react-intl";
 import LocaleContext from './LocaleContext';
 import useTranslation from '../useTranslation';
 
-export function LocaleProvider({locale, messages: customMessages, ...props}) {
+export function LocaleProvider({locale, messages: customMessages = {}, ...props}) {
   const { messages, localeMap, loading, error } = useTranslation({ locale });
   return <LocaleContext.Provider value={{localeMap}}>
-    <IntlProvider locale={locale} messages={customMessages || messages} {...props} />
+    <IntlProvider locale={locale} messages={{...messages, ...customMessages}} {...props} />
   </LocaleContext.Provider>
 }
