7b38109e1f107bf722b7f9c33d7a4d7268db8e3b
Morten Høfft <mhoefft@gbif.org>
2022-03-09 15:32:18 +0100

    update config to new schema

--- packages/react-components/src/search/CollectionSearch/Standalone.js
+++ packages/react-components/src/search/CollectionSearch/Standalone.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext } from "react";
 import StandaloneWrapper from '../../StandaloneWrapper';
 import { Switch, Route } from 'react-router-dom';
 import CollectionSearch from './CollectionSearch';
+import RouteContext from '../../dataManagement/RouteContext';
 
-function Standalone({siteConfig = {}, ...props}) {
-  const path = window.location.pathname;
-  return <StandaloneWrapper {...props}>
-    <Switch>
-      <Route
-        path={path}
-        render={routeProps => <CollectionSearch pageLayout config={siteConfig.collection} {...props} />}
-      />
-    </Switch>
+function Wrap({ siteConfig, ...props }) {
+  return <StandaloneWrapper siteConfig={siteConfig}>
+    <Standalone {...props} siteConfig={siteConfig}/>
   </StandaloneWrapper>
 }
 
-export default Standalone;
+function Standalone(props) {
+  const routeContext = useContext(RouteContext);
+  const path = routeContext.collectionSearch.route;
+  return <Switch>
+    <Route
+      path={path}
+      render={routeProps => <CollectionSearch pageLayout config={props?.siteConfig?.collection} {...props} {...routeProps} />}
+    />
+  </Switch>
+}
+
+export default Wrap;
