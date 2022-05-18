3f252d4ab0e1ba8e3f346c27968d2983b7023e6f
Dave Martin <djtfmartin@gmail.com>
2022-05-18 13:08:54 +0100

    event tree display

---
+++ packages/react-components/src/entities/EventSidebar/details/Tree/Tree.js
@@ -0,0 +1,73 @@
+import React, {Component, useContext} from 'react';
+import styles from './styles';
+import {Row} from "../../../../components";
+import ThemeContext from "../../../../style/themes/ThemeContext";
+
+class TreeNode {
+  constructor(key, value = key, count, parent = null, ch) {
+    this.key = key;
+    this.value = value;
+    this.count = count;
+    this.parent = parent;
+    this.children = [];
+  }
+}
+
+export function TreeN({ treeNode }) {
+  return <ul>
+    <li>
+      <a href="#">{ treeNode.value }</a>
+      { treeNode.children.map(childNode => <TreeN treeNode={childNode}/>) }
+    </li>
+  </ul>
+}
+
+export function Tree({
+  data = {},
+  loading,
+  error,
+  className,
+  ...props
+}) {
+
+  const rootNode = new TreeNode("Dataset", "Dataset", null, []);
+
+  data.map(branch => {
+
+    let currentNode = rootNode;
+
+    const verticies = branch.key.split('/').map(s => s.trim());
+    const count = branch.count;
+
+    verticies.map((vertex, idx) => {
+
+      // does node exist already in children ?
+      let node = null;
+
+      currentNode.children.forEach((childNode, i) => {
+        if (childNode.value === vertex){
+          // already exists
+          node = childNode;
+          node.count = node.count + childNode.count;
+        }
+      });
+
+      if (node == null){
+        // create new node, add to parent
+        node = new TreeNode(vertex, vertex, count, currentNode, []);
+        currentNode.children.push(node);
+      }
+
+      currentNode = node;
+    })
+  });
+
+  const theme = useContext(ThemeContext);
+
+  return <Row>
+    <div css={styles.tree({ theme })}>
+        <TreeN treeNode={rootNode} />
+    </div>
+  </Row>
+
+};
