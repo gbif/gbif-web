ffed8be273d725f8530154b8a41a3f295461eb30
Morten Høfft <mhoefft@gbif.org>
2022-05-19 21:29:48 +0200

    test build

--- packages/react-components/webpack.config.js
+++ packages/react-components/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const pkg = require('./package.json');
 
-module.exports = {
+const browserConfig = {
+  name: 'browser',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
@@ -29,3 +31,36 @@ module.exports = {
     ]
   }
 };
+
+const nodeConfig = {
+  name: 'node',
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build/esm'),
+    filename: 'index.js',
+    library: 'gbifReactComponents',
+    libraryTarget: 'commonjs2',
+  },
+  externals: {
+    'react': 'React', // Case matters here 
+    'react-dom': 'ReactDOM', // Case matters here
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ['transform-react-remove-prop-types', '@babel/plugin-transform-runtime'],
+            presets: ["@babel/preset-env", "@babel/preset-react", "@emotion/babel-preset-css-prop"]
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = [browserConfig, nodeConfig];
