d98abf755e019b0a4b57c31bbffd451f70d74723
Dave Martin <djtfmartin@gmail.com>
2022-07-21 18:05:06 +0100

    more minor UI improvements for events

--- packages/react-components/src/entities/EventSidebar/EventSidebar.js
+++ packages/react-components/src/entities/EventSidebar/EventSidebar.js
@@ -4,6 +4,8 @@ import * as css from './styles';
 import { Row, Col, Tabs } from "../../components";
 import { useQuery } from '../../dataManagement/api';
 import { Intro } from './details/Intro';
+import {MdClose, MdInfo} from "react-icons/md";
+const { TabList, Tab, TapSeperator } = Tabs;
 
 const {  TabPanel } = Tabs;
 
@@ -33,17 +35,37 @@ export function EventSidebar({
     }
   }, [data, loading]);
 
+  const isLoading = loading || !data;
+
   return <Tabs activeId={activeId} onChange={id => setTab(id)}>
     <Row wrap="nowrap" style={style} css={css.sideBar({ theme })}>
+      <Col shrink={false} grow={false} css={css.detailDrawerBar({ theme })}>
+        <TabList style={{ paddingTop: '12px' }} vertical>
+          {onCloseRequest && <>
+            <Tab direction="left" onClick={onCloseRequest}>
+              <MdClose />
+            </Tab>
+            <TapSeperator vertical />
+          </>}
+          <Tab tabId="details" direction="left">
+            <MdInfo />
+          </Tab>
+        </TabList>
+      </Col>
       <Col shrink={false} grow={false} css={css.detailDrawerContent({ theme })} >
-        <TabPanel tabId='details' style={{height: '100%'}}>
-          <Intro
-              data={data}
-              loading={loading}
-              error={error}
-              setActiveEventID={setActiveEventID}
-          />
-        </TabPanel>
+        {isLoading && <Col style={{ padding: '12px', paddingBottom: 50, overflow: 'auto' }} grow>
+          <h2>{eventID} - Loading event information...</h2>
+        </Col>}
+        {!isLoading &&
+            <TabPanel tabId='details' style={{height: '100%'}}>
+              <Intro
+                  data={data}
+                  loading={loading}
+                  error={error}
+                  setActiveEventID={setActiveEventID}
+              />
+            </TabPanel>
+        }
       </Col>
     </Row>
   </Tabs>
--- packages/react-components/src/entities/SiteSidebar/SiteSidebar.js
+++ packages/react-components/src/entities/SiteSidebar/SiteSidebar.js
@@ -8,6 +8,8 @@ import { FormattedMessage } from "react-intl";
 import { EnumField, PlainTextField } from "../EventSidebar/details/properties";
 import Map from "./details/Map/Map";
 import { Summary } from "./details/Summary";
+import {MdClose, MdInfo} from "react-icons/md";
+const { TabList, Tab, TapSeperator } = Tabs;
 
 export function SiteSidebar({
   onCloseRequest,
@@ -38,10 +40,25 @@ export function SiteSidebar({
 
   return <Tabs activeId={activeId} onChange={id => setTab(id)}>
     <Row wrap="nowrap" style={style} css={css.sideBar({ theme })}>
+      <Col shrink={false} grow={false} css={css.detailDrawerBar({ theme })}>
+        <TabList style={{ paddingTop: '12px' }} vertical>
+          {onCloseRequest && <>
+            <Tab direction="left" onClick={onCloseRequest}>
+              <MdClose />
+            </Tab>
+            <TapSeperator vertical />
+          </>}
+          <Tab tabId="details" direction="left">
+            <MdInfo />
+          </Tab>
+        </TabList>
+      </Col>
       <Col shrink={false} grow={false} css={css.detailDrawerContent({ theme })} >
         <TabPanel tabId='details' style={{ height: '100%' }}>
           <Row direction="column" wrap="nowrap" style={{ maxHeight: '100%', overflow: 'hidden' }}>
-            {isLoading && <h2>Loading site information for {siteID} </h2>}
+            {isLoading && <Col style={{ padding: '12px', paddingBottom: 50, overflow: 'auto' }} grow>
+              <h2>Site: {siteID} - Loading information...</h2>
+            </Col>}
             {!isLoading && <Col style={{ padding: '12px', paddingBottom: 50, overflow: 'auto' }} grow>
               <h2> Site: {siteID} </h2>
               <Group label={"Site location"}>
