a0947252d8a37af9c99563a23050a428f3612450
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2022-09-21 15:49:58 +0200

    formatting, and use markdownit instead.

--- packages/react-components/src/components/HyperText/HyperText.js
+++ packages/react-components/src/components/HyperText/HyperText.js
@@ -2,52 +2,59 @@ import React from 'react';
 import { jsx } from '@emotion/react';
 import Autolinker from 'autolinker';
 import DOMPurify from 'dompurify';
-import snarkdown from 'snarkdown';
+import MarkdownIt from 'markdown-it';
 import doiRegex from 'doi-regex';
 import { Doi } from '../IdentifierBadge';
 import Orcid from '../Orcid/Orcid';
 import Lsid from '../Lsid/Lsid';
 import BooleanValue from '../BooleanValue/BooleanValue'
-import {content} from './styles'
+import { content } from './styles'
+
+const md = MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: false
+});
+md.linkify.tlds(['org', 'com'], false);
 
 const autolinker = new Autolinker(
     {
-    truncate: { length: 64, location: 'middle' }, 
-    stripPrefix: false,
-    email: false, 
-    phone: false
-});
+        truncate: { length: 64, location: 'middle' },
+        stripPrefix: false,
+        email: false,
+        phone: false
+    });
 
 const getLsid = (text) => {
-    if(typeof text !== "string" || /\s/.test(text.trim())){
+    if (typeof text !== "string" || /\s/.test(text.trim())) {
         return null
     } else {
         const trimmed = text.trim();
         if (trimmed.startsWith('urn:lsid:')) {
-            return trimmed; 
-        } else {
-            return null
-        }
-    } 
-}
-
-const getOrcid = (text) => {
-    if(typeof text !== "string" || /\s/.test(text.trim())){
-        return null
-    } else {
-        const trimmed = text.trim();
-        if (trimmed.startsWith('orcid.org/') ) {
-            return 'https://' + trimmed;
-        } else if(trimmed.startsWith('https://orcid.org/') || trimmed.startsWith('http://orcid.org/')){
             return trimmed;
         } else {
             return null
         }
-    } 
+    }
+}
+
+const getOrcid = (text) => {
+    if (typeof text !== "string" || /\s/.test(text.trim())) {
+        return null
+    } else {
+        const trimmed = text.trim();
+        if (trimmed.startsWith('orcid.org/')) {
+            return 'https://' + trimmed;
+        } else if (trimmed.startsWith('https://orcid.org/') || trimmed.startsWith('http://orcid.org/')) {
+            return trimmed;
+        } else {
+            return null
+        }
+    }
 }
 
 const getDoi = (text) => {
-    if(typeof text !== "string" || /\s/.test(text.trim())){
+    if (typeof text !== "string" || /\s/.test(text.trim())) {
         return null
     } else {
         const doi = text.trim().match(doiRegex())?.[0];
@@ -56,31 +63,31 @@ const getDoi = (text) => {
         } else {
             return null;
         }
-    } 
+    }
 }
 
-export const HyperText = ({text, parseMarkdown, inline, addLinks = true, linkOptions, sanitizeOptions, ...props}) =>  {
-    if(text === false || text === true){
-        return <BooleanValue value={text} {...props}/>
+export const HyperText = ({ text, parseMarkdown, inline, addLinks = true, linkOptions, sanitizeOptions, ...props }) => {
+    if (text === false || text === true) {
+        return <BooleanValue value={text} {...props} />
     }
-    if(typeof text === "undefined"){
+    if (typeof text === "undefined") {
         return null;
     }
-    if(typeof text !== "string"){
+    if (typeof text !== "string") {
         return <span {...props}>{text}</span>;
     }
 
     const doi = getDoi(text);
-    if(doi){
-        return <Doi id={doi} {...props}/>
+    if (doi) {
+        return <Doi id={doi} {...props} />
     }
     const orcid = getOrcid(text);
-    if(orcid){
-        return <Orcid href={orcid} {...props}/>
+    if (orcid) {
+        return <Orcid href={orcid} {...props} />
     }
     const lsid = getLsid(text);
-    if(lsid){
-        return <Lsid identifier={lsid} {...props}/>
+    if (lsid) {
+        return <Lsid identifier={lsid} {...props} />
     }
 
     let html = text;
@@ -88,7 +95,7 @@ export const HyperText = ({text, parseMarkdown, inline, addLinks = true, linkOpt
     if (addLinks) {
         if (linkOptions) {
             // use custom config
-            html = Autolinker.link(html, linkOptions);    
+            html = Autolinker.link(html, linkOptions);
         } else {
             //use default link config
             html = autolinker.link(html);
@@ -96,10 +103,11 @@ export const HyperText = ({text, parseMarkdown, inline, addLinks = true, linkOpt
     }
 
     if (parseMarkdown) {
-        html = snarkdown(html);
+        if (inline) html = md.renderInline(html);
+        else html = md.render(html);
     }
     const sanitized = DOMPurify.sanitize(html, sanitizeOptions);
 
-    
-    return <div css={content({inline})} dangerouslySetInnerHTML={{ __html: sanitized }} {...props}></div>
+
+    return <div css={content({ inline })} dangerouslySetInnerHTML={{ __html: sanitized }} {...props}></div>
 }
