4b156203f0162193549d5706b9df37729d55b2d8
Jack Brinkman <jack.brinkman@csiro.au>
2022-11-23 11:04:46 +1000

    #206 Added locale support

--- packages/react-components/src/components/ErrorBoundary/ErrorBoundary.js
+++ packages/react-components/src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,6 +1,7 @@
 import * as styles from './ErrorBoundary.styles';
 import { Button } from '../Button';
 import { MdError } from 'react-icons/md';
+import { FormattedMessage } from 'react-intl';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -31,11 +32,22 @@ class ErrorBoundary extends React.Component {
     return error ? (
       <div css={styles.container}>
         <MdError size={72} />
-        <h1 style={{ marginBottom: 0 }}>An error occurred</h1>
-        <h3 style={{ marginTop: 12 }}>{error.message || 'Unknown error'}</h3>
+        <h1 style={{ marginBottom: 0 }}>
+          <FormattedMessage id="error.generic" defaultMessage="An error occurred" />
+        </h1>
+        <h3 style={{ marginTop: 12 }}>
+          {error.message || <FormattedMessage id="error.unknown" defaultMessage="Unknown error" />}
+        </h3>
         <div css={styles.actions}>
-          <Button as="a" target="_blank" href="https://github.com/gbif/gbif-web/issues/new">Report to GBIF</Button>
-          <Button onClick={this.handleToggleStack}>{showStack ? 'Hide' : 'Show'} Details</Button>
+          <Button as="a" target="_blank" href="https://github.com/gbif/gbif-web/issues/new">
+            <FormattedMessage id="error.report" defaultMessage="Report issue" />
+          </Button>
+          <Button onClick={this.handleToggleStack}>
+            <FormattedMessage
+              id={showStack ? 'error.hideDetails' : 'error.showDetails'}
+              defaultMessage={showStack ? 'Hide details' : 'Show details'}
+            />
+          </Button>
         </div>
         {error.stack && showStack && (
           <div css={styles.stack}>
