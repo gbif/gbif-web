51a98ae9d6087db3a2dad383d111746d6dd69c20
Jack Brinkman <jack.brinkman@csiro.au>
2022-11-30 13:56:15 +1000

    GenIcon fix, updated build script

--- packages/react-components/build.mjs
+++ packages/react-components/build.mjs
@@ -2,6 +2,8 @@ import { analyzeMetafile, build } from 'esbuild';
 import fsp from 'fs/promises';
 import GlobalsPlugin from 'esbuild-plugin-globals';
 
+const OUTPUT_META = false;
+
 const shared = {
   bundle: true,
   logLevel: 'info',
@@ -21,7 +23,7 @@ const configs = [
     format: 'esm',
     entryPoints: ['src/index.js'],
     outfile: `./dist/index.esm.js`,
-    metafile: false,
+    metafile: OUTPUT_META,
   },
   {
     format: 'cjs',
@@ -41,24 +43,6 @@ const configs = [
       }),
     ],
   },
-  // {
-  //   format: 'esm',
-  //   entryPoints: {
-  //     CollectionSearch: 'src/search/CollectionSearch/Standalone.js',
-  //     DatasetSearch: 'src/search/DatasetSearch/Standalone.js',
-  //     EventSearch: 'src/search/EventSearch/Standalone.js',
-  //     InstitutionSearch: 'src/search/InstitutionSearch/Standalone.js',
-  //     LiteratureSearch: 'src/search/LiteratureSearch/Standalone.js',
-  //     OccurrenceSearch: 'src/search/OccurrenceSearch/Standalone.js',
-  //     PublisherSearch: 'src/search/PublisherSearch/Standalone.js',
-  //     Collection: 'src/entities/Collection/Standalone.js',
-  //     Institution: 'src/entities/Institution/Standalone.js',
-  //     Dataset: 'src/entities/Dataset/Standalone.js',
-  //     themeBuilder: 'src/style/themeBuilder/index.js',
-  //   },
-  //   external: ['react', 'react-dom'],
-  //   outdir: './dist/esm',
-  // },
 ];
 
 configs.forEach(async (configPart) => {
@@ -66,7 +50,7 @@ configs.forEach(async (configPart) => {
     ...shared,
     ...configPart,
   });
-  if (configPart.format === 'esm') {
+  if (OUTPUT_META && configPart.format === 'esm') {
     await fsp.writeFile(
       './analysis.txt',
       await analyzeMetafile(result.metafile, {
