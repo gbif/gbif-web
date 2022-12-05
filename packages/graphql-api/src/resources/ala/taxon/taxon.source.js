367bf3782da3256f169000712fd6457f8e7a3413
Jack Brinkman <jack.brinkman@csiro.au>
2022-12-05 12:36:24 +1000

    Switched config logic to use ALA BIE by default

--- packages/graphql-api/src/resources/ala/taxon/taxon.source.js
+++ packages/graphql-api/src/resources/ala/taxon/taxon.source.js
@@ -3,46 +3,47 @@ import { RESTDataSource } from 'apollo-datasource-rest';
 class TaxonAPI extends RESTDataSource {
   constructor(config) {
     super();
-    this.baseURL = config.useBie ? config.alaBie : config.apiEs;
-    this.useBie = config.useBie;
+    this.baseURL = config.useOccTaxa ? config.apiEs : config.alaBie;
+    this.useOccTaxa = config.useOccTaxa;
   }
 
   async getTaxonByKey({ key }) {
-    if (this.useBie) {
-      const { taxonConcept, classification } = await this.get(
-        `/species/${key}.json`,
+    // Temporarily use the event-occurrence endpoint rather than the BIE
+    // if specified in the config
+    if (this.useOccTaxa) {
+      const { documents } = await this.get(
+        `/event-occurrence?apiKey=my-api-key&size=1&gbifClassification_acceptedUsage_key=${key}`,
       );
+      const [result] = documents.results;
+
       return {
+        ...result,
         key,
-        rank: taxonConcept.rankString,
-        scientificName: classification.scientificName,
-        kingdom: classification.kingdom,
-        kingdomKey: classification.kingdomGuid,
-        phylum: classification.phylum,
-        phylumKey: classification.phylumGuid,
-        class: classification.class,
-        classKey: classification.classGuid,
-        order: classification.order,
-        orderKey: classification.orderGuid,
-        family: classification.family,
-        familyKey: classification.familyGuid,
-        genus: classification.genus,
-        genusKey: classification.genusGuid,
-        species: classification.species,
-        speciesKey: classification.speciesGuid,
+        scientificName: result.acceptedScientificName,
+        rank: result.gbifClassification.acceptedUsage.rank,
       };
     }
 
-    const { documents } = await this.get(
-      `/event-occurrence?apiKey=my-api-key&size=1&gbifClassification_acceptedUsage_key=${key}`,
-    );
-    const [result] = documents.results;
-
+    // Call the BIE api & return the taxonomic data
+    const { taxonConcept, classification } = await this.get(`/species/${key}`);
     return {
-      ...result,
       key,
-      scientificName: result.acceptedScientificName,
-      rank: result.gbifClassification.acceptedUsage.rank,
+      rank: taxonConcept.rankString,
+      scientificName: classification.scientificName,
+      kingdom: classification.kingdom,
+      kingdomKey: classification.kingdomGuid,
+      phylum: classification.phylum,
+      phylumKey: classification.phylumGuid,
+      class: classification.class,
+      classKey: classification.classGuid,
+      order: classification.order,
+      orderKey: classification.orderGuid,
+      family: classification.family,
+      familyKey: classification.familyGuid,
+      genus: classification.genus,
+      genusKey: classification.genusGuid,
+      species: classification.species,
+      speciesKey: classification.speciesGuid,
     };
   }
 }
