74778b5b2186e5cf4dc1c30535cbf4ce35ddff49
Jack Brinkman <jack.brinkman@csiro.au>
2022-12-05 12:43:10 +1000

    Temporarily update taxon source

--- packages/graphql-api/src/resources/ala/taxon/taxon.source.js
+++ packages/graphql-api/src/resources/ala/taxon/taxon.source.js
@@ -3,13 +3,29 @@ import { RESTDataSource } from 'apollo-datasource-rest';
 class TaxonAPI extends RESTDataSource {
   constructor(config) {
     super();
-    this.baseURL = config.alaBie;
+    this.baseURL = config.useOccTaxa ? config.apiEs : config.alaBie;
+    this.useOccTaxa = config.useOccTaxa;
   }
 
   async getTaxonByKey({ key }) {
-    const { taxonConcept, classification } = await this.get(
-      `/species/${key}.json`,
-    );
+    // Temporarily use the event-occurrence endpoint rather than the BIE
+    // if specified in the config
+    if (this.useOccTaxa) {
+      const { documents } = await this.get(
+        `/event-occurrence?apiKey=my-api-key&size=1&gbifClassification_acceptedUsage_key=${key}`,
+      );
+      const [result] = documents.results;
+
+      return {
+        ...result,
+        key,
+        scientificName: result.acceptedScientificName,
+        rank: result.gbifClassification.acceptedUsage.rank,
+      };
+    }
+
+    // Call the BIE api & return the taxonomic data
+    const { taxonConcept, classification } = await this.get(`/species/${key}`);
     return {
       key,
       rank: taxonConcept.rankString,
