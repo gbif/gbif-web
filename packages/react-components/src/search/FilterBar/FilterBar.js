385aa7882882f4d02d4742ea5199ed2607afe74f
QBai <31238531+qifeng-bai@users.noreply.github.com>
2023-03-09 10:59:23 +1100

    https://github.com/AtlasOfLivingAustralia/extended-data-model/issues/68 Add filter reset button

--- packages/react-components/src/search/FilterBar/FilterBar.js
+++ packages/react-components/src/search/FilterBar/FilterBar.js
@@ -3,9 +3,12 @@ import { css, jsx } from '@emotion/react';
 import React, { useContext } from 'react';
 import get from 'lodash/get';
 import union from 'lodash/union';
-import { withFilter } from '../../widgets/Filter/state';
+import { FilterContext, withFilter } from '../../widgets/Filter/state';
 import ThemeContext from '../../style/themes/ThemeContext';
 import { Trigger as MetaFilter } from '../../widgets/Filter/types/MetaFilter';
+import { Button, Tooltip } from '../../components';
+import { MdDelete } from 'react-icons/md';
+import SiteContext from '../../dataManagement/SiteContext';
 
 function getVisibleFilters(currentFilter, commonFilters) {
   const visibleFilters = union(commonFilters,
@@ -21,21 +24,42 @@ const FilterBar = ({
   ...props
 }) => {
   const theme = useContext(ThemeContext);
+  const currentFilterContext = useContext(FilterContext);
+  const {event: eventConfig} = useContext(SiteContext);
+
   const prefix = theme.prefix || 'gbif';
   const elementName = 'filterBar';
 
   const visibleFilters = getVisibleFilters(filter, config.defaultVisibleFilters);
   const availableFilters = visibleFilters.map(x => config.filters[x]);
+  const currentFilters = Object.keys({
+    ...(currentFilterContext.filter.must || {}),
+    ...(currentFilterContext.filter.must_not || {})
+  });
 
   return <div className={`${className} ${prefix}-${elementName}`} css={css`${style(theme)}`} {...props}>
-    {availableFilters.map((x, i) => {
-      if (!x) return null; // if no widget is defined for this filter, then do not show anything
-      return <x.Button key={i} />
-    })}
-    {Object.keys(config.filters).length !== config.defaultVisibleFilters.length &&
     <div>
-      <MetaFilter />
-    </div>}
+      {availableFilters.map((x, i) => {
+        if (!x) return null; // if no widget is defined for this filter, then do not show anything
+        return <x.Button key={i}/>
+      })}
+      {Object.keys(config.filters).length !== config.defaultVisibleFilters.length && (
+        <div>
+          <MetaFilter />
+        </div>
+      )}
+    </div>
+    {currentFilters.length > 0 && eventConfig?.enableResetFilter && (
+      <Tooltip title="Clear all filters" placement="auto">
+        <Button
+          onClick={() => currentFilterContext.setFilter({})}
+          appearance="link"
+          css={css`margin-right: 20px;`}
+        >
+          <MdDelete css={css`margin-top:8px; font-size: 20px;`} />
+        </Button>
+      </Tooltip>
+    )}
   </div>
 }
 
@@ -45,9 +69,16 @@ FilterBar.propTypes = {
 export const style = (theme) => css`
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  margin-bottom: -4px;
-  >div {
+  align-items: flex-start;
+  justify-content: space-between;
+  width: 100%;
+  > div {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-bottom: -4px;
+  }
+  > div > div {   
     max-width: 100%;
     margin-right: 4px; 
     margin-bottom: 4px;
