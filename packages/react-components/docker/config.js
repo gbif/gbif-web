9ad674ec396add59fe86ed69cab426869c31b380
Q.B <qifeng.bai@csiro.au>
2023-03-08 12:03:41 +1100

    remove docker for ALA

--- packages/react-components/docker/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    baseUrl: '{{HOST_SERVER}}',
-    authServer: 'https://auth.ala.org.au/cas/oidc',
-    authClientID: '{{AUTH_CLIENT_ID}}',
-    collectionsServer: "https://collections-test.ala.org.au"
-}
