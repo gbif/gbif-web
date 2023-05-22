23a126a990a12bec30fef7eb73c8acc5cb4f9c09
Dave Martin <djtfmartin@gmail.com>
2023-05-22 15:57:53 +0100

    Changes pulled from ala-events Removed context from graphql code

--- packages/react-components/ala-config.js
+++ packages/react-components/ala-config.js
@@ -1,6 +1,16 @@
 const config = {
-    baseUrl: 'https://events-test.ala.org.au/',
-    authServer: 'https://auth.ala.org.au/cas/oidc',
-    authClientID: 'events-test-oidc-auth-prod',
+    baseUrl: 'https://events.test.ala.org.au/',
+    authServer: 'https://auth-test.ala.org.au/',
+    authClientID: 'oidc-events-test',
     collectionsServer: "https://collections-test.ala.org.au"
 }
+
+// Production
+// const config = {
+//     baseUrl: 'https://events.ala.org.au/',
+//     authServer: 'https://auth.ala.org.au/',
+//     authClientID: 'events-oidc-auth-prod',
+//     collectionsServer: "https://collections.ala.org.au"
+// }
+
+
