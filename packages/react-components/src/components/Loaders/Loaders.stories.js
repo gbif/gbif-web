325f091c72725b6f6820a4d904ed70da3a89f28c
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-06-19 13:22:51 +0200

    feat: additional loader

--- packages/react-components/src/components/Loaders/Loaders.stories.js
+++ packages/react-components/src/components/Loaders/Loaders.stories.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { boolean } from '@storybook/addon-knobs';
 import { StripeLoader } from './StripeLoader';
+import { CircularLoader } from './CircularLoader';
 
 export default {
-  title: 'Components/StripeLoader',
+  title: 'Components/Loaders',
   component: StripeLoader,
 };
 
-export const Example = () => <div style={{padding: 20, background: 'white'}}>
-  <StripeLoader active={boolean("active", true)} error={boolean("error", false)}/>
+export const Example = () => <div style={{ padding: 20, background: 'white' }}>
+  <StripeLoader active={boolean("active", true)} error={boolean("error", false)} />
+</div>
+
+export const Circular_Loader = () => <div style={{ height: '2rem', width: '2rem' }}>
+  <CircularLoader active={boolean("active", true)} />
 </div>
 
 Example.story = {
-  name: 'StripeLoader',
+  name: 'Stripe Loader',
 };
--- packages/react-components/src/components/Loaders/index.js
+++ packages/react-components/src/components/Loaders/index.js
@@ -1,1 +1,2 @@
 export * from './StripeLoader';
+export * from './CircularLoader';
