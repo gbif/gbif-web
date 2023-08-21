e2a521e1c82222ef8bde62e2ca8a2cd23f5311f4
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-08-21 11:25:03 +0200

    style: wrap prose text

--- packages/react-components/src/components/typography/Prose.js
+++ packages/react-components/src/components/typography/Prose.js
@@ -101,7 +101,7 @@ export const a = (theme) => css`
 export const prose = ({ theme = {} }) => css`
   -webkit-font-smoothing: antialiased;
   line-height: 1.6em;
-  /* line-break: anywhere; might be relevant on smaller devices*/
+  overflow-wrap: anywhere;
   a {
     /* ${a(theme)}; */
     color: var(--linkColor);
