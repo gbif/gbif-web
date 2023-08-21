26486c5b1e2bfbc656916087e734b65942a6ba00
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-08-21 14:14:23 +0200

    fix: use env for map tiles

--- packages/react-components/src/entities/Dataset/about/details/ThumbnailMap.js
+++ packages/react-components/src/entities/Dataset/about/details/ThumbnailMap.js
@@ -2,6 +2,7 @@ import { jsx } from '@emotion/react';
 import React, { useContext } from 'react';
 import ThemeContext from '../../../../style/themes/ThemeContext';
 import * as css from '../styles';
+import env from '../../../../../.env.json';
 
 export function ThumbnailMap({
   dataset,
@@ -9,12 +10,12 @@ export function ThumbnailMap({
 }) {
   return <div css={css.thumbnail}>
     <div>
-      <img src={`https://tile.gbif.org/4326/omt/0/0/0@1x.png?style=gbif-light&srs=EPSG%3A4326`} />
-      <img src={`https://tile.gbif.org/4326/omt/0/1/0@1x.png?style=gbif-light&srs=EPSG%3A4326`} />
+      <img src={`${env.BASEMAPS}/4326/omt/0/0/0@1x.png?style=gbif-light&srs=EPSG%3A4326`} />
+      <img src={`${env.BASEMAPS}/4326/omt/0/1/0@1x.png?style=gbif-light&srs=EPSG%3A4326`} />
     </div>
     <div>
-      <img src={`https://api.gbif.org/v2/map/occurrence/density/0/0/0@2x.png?style=classic.poly&bin=hex&hexPerTile=79&datasetKey=${dataset.key}&srs=EPSG%3A4326`} onError={i => i.target.style.visibility ='hidden'} />
-      <img src={`https://api.gbif.org/v2/map/occurrence/density/0/1/0@2x.png?style=classic.poly&bin=hex&hexPerTile=79&datasetKey=${dataset.key}&srs=EPSG%3A4326`} onError={i => i.target.style.visibility ='hidden'} />
+      <img src={`${env.API_V2}/map/occurrence/density/0/0/0@2x.png?style=classic.poly&bin=hex&hexPerTile=79&datasetKey=${dataset.key}&srs=EPSG%3A4326`} onError={i => i.target.style.visibility ='hidden'} />
+      <img src={`${env.API_V2}/map/occurrence/density/0/1/0@2x.png?style=classic.poly&bin=hex&hexPerTile=79&datasetKey=${dataset.key}&srs=EPSG%3A4326`} onError={i => i.target.style.visibility ='hidden'} />
     </div>
   </div>
 };
