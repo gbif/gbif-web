c7d5b50b861351f20451eeb9dad6aab46b0f809d
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2023-10-30 10:55:09 +0100

    Fixed inconsistent indentation

--- packages/es-api/src/middleware/errorLoggingMiddleware.js
+++ packages/es-api/src/middleware/errorLoggingMiddleware.js
@@ -1,28 +1,28 @@
 const logger = require('../logger');
 
 function errorLoggingMiddleware(error, req, res, next) {
-    // Mark that the error has been logged so that the logging middleware doesn't log it again
-    req.loggedAsError = true;
+	// Mark that the error has been logged so that the logging middleware doesn't log it again
+	req.loggedAsError = true;
 
-    const date = new Date();
+	const date = new Date();
 
-    logger.error({
-        message: 'ES-API Error',
-        error,
-        time: date.toISOString(),
-        timeInCopenhagen: date.toLocaleString('da-DK', { timeZone: 'Europe/Copenhagen' }),
-        request: {
-            headers: req.headers,
-            body: req.body,
-            method: req.method,
-            url: req.url,
-        },
-        response: {
-            statusCode: res.statusCode,
-            headers: res.headers,
-            body: res.body,
-        }
-    });
+	logger.error({
+		message: 'ES-API Error',
+		error,
+		time: date.toISOString(),
+		timeInCopenhagen: date.toLocaleString('da-DK', { timeZone: 'Europe/Copenhagen' }),
+		request: {
+			headers: req.headers,
+			body: req.body,
+			method: req.method,
+			url: req.url,
+		},
+		response: {
+			statusCode: res.statusCode,
+			headers: res.headers,
+			body: res.body,
+		}
+	});
 }
 
 module.exports = errorLoggingMiddleware;
--- packages/es-api/src/middleware/index.js
+++ packages/es-api/src/middleware/index.js
@@ -2,6 +2,6 @@ const loggingMiddleware = require('./loggingMiddleware');
 const errorLoggingMiddleware = require('./errorLoggingMiddleware');
 
 module.exports = {
-    loggingMiddleware,
-    errorLoggingMiddleware,
+  loggingMiddleware,
+  errorLoggingMiddleware,
 }
