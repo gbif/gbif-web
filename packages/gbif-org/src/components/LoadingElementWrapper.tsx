45d58fdc62519d1fca57e88bd144acc9680ce89f
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2023-11-17 14:49:29 +0100

    Add better comments

--- packages/gbif-org/src/components/LoadingElementWrapper.tsx
+++ packages/gbif-org/src/components/LoadingElementWrapper.tsx
@@ -11,9 +11,10 @@ type Props = {
 export function LoadingElementWrapper({ children, nestingLevel, lang, id }: Props) {
   const loadingElement = useLoadingElement(nestingLevel, lang, id);
 
+  // Dispatch the done rendering event when the component has mounted.
+  // This will let the loading element context know that the new route has mounted and the loading element can be removed.
   React.useEffect(() => {
     if (typeof window === 'undefined') return;
-
     window.dispatchEvent(new DoneRenderingEvent({ id }));
   }, [loadingElement]);
 
