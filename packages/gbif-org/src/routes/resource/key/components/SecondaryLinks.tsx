840757c9e7a8e836f070e7ea4e8b2a9ccb469c9e
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2024-01-13 10:06:06 +1000

    Allow secondaryLinks without a label

--- packages/gbif-org/src/routes/resource/key/components/SecondaryLinks.tsx
+++ packages/gbif-org/src/routes/resource/key/components/SecondaryLinks.tsx
@@ -1,28 +1,34 @@
 import { cn } from '@/utils/shadcn';
-import { MdInsertLink, MdLink } from 'react-icons/md';
+import { MdLink } from 'react-icons/md';
+
+type NullableLink = null | {
+  label?: string | null;
+  url?: string | null;
+};
+
+type LinkWithUrl = {
+  label?: string | null;
+  url: string;
+};
 
 type Props = {
-  links: Array<null | {
-    label?: string | null;
-    url?: string | null;
-  }>;
+  links: Array<NullableLink>;
   className?: string;
 };
 
 export function SecondaryLinks({ links, className }: Props) {
   return (
     <ul className={cn('grid grid-cols-2 gap-4', className)}>
-      {links.filter(isValidLink).map((link, index) => (
+      {links.filter(isLinkWithUrl).map((link) => (
         <li key={link.url} className="border p-3 shadow-sm hover:shadow-md">
           <a
             className="flex flex-row items-center underline"
-            target="_blank" rel="noopener noreferrer"
+            target="_blank"
+            rel="noopener noreferrer"
             href={link.url}
           >
             <MdLink />
-            <div className="ms-4 font-medium">
-              {link?.label ?? link?.url}
-            </div>
+            <div className="ms-4 font-medium">{link?.label ?? link.url}</div>
           </a>
         </li>
       ))}
@@ -30,12 +36,7 @@ export function SecondaryLinks({ links, className }: Props) {
   );
 }
 
-function isValidLink(
-  link: null | {
-    label?: string | null;
-    url?: string | null;
-  }
-): link is { label: string; url: string } {
+function isLinkWithUrl(link: NullableLink): link is LinkWithUrl {
   if (link == null) return false;
-  return link.label != null && link.url != null;
+  return link.url != null;
 }
