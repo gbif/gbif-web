462bc7c2a1d8e6b6d5e1e01c823525ea0aab8ee2
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-01-19 10:54:58 +0100

    generic article skeleton

--- packages/gbif-org/src/routes/resource/key/components/ArticlePreTitle.tsx
+++ packages/gbif-org/src/routes/resource/key/components/ArticlePreTitle.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/utils/shadcn';
+import { Skeleton } from '@/components/ui/skeleton';
 
 type Props = {
   children: React.ReactNode;
@@ -16,5 +17,13 @@ export function ArticlePreTitle({ secondary, children, className }: Props) {
     >
       {children} {secondary && <span className="text-slate-500 font-normal ml-2">{secondary}</span>}
     </p>
+  );
+}
+
+export function ArticlePreTitleSkeleton({ className }: Pick<Props, 'className'>) {
+  return (
+    <div className={className}>
+      <Skeleton className="w-10 h-4" />
+    </div>
   );
 }
