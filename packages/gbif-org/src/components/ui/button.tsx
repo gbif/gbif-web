88a4b3f5871def5e94b4ef095ad6431cef5a298d
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2024-02-28 14:53:41 +0100

    Add gb-button--brand

--- packages/gbif-org/src/components/ui/button.tsx
+++ packages/gbif-org/src/components/ui/button.tsx
@@ -5,11 +5,11 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/shadcn';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'gb-button',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primaryContrast shadow hover:bg-primary/90',
+        default: 'gb-button-variant-default',
         destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
           'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         linkDestructive: 'text-destructive underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'gb-button-size-default',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
