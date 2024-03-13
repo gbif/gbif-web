daa94ae2c92462241cac8597b64b7321609e9a09
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2024-03-13 16:58:03 +0100

    composition Block rendering: Respect card counts #475

--- packages/gbif-org/src/routes/resource/key/composition/blocks/FeatureBlock.tsx
+++ packages/gbif-org/src/routes/resource/key/composition/blocks/FeatureBlock.tsx
@@ -5,6 +5,7 @@ import { ArticleBody } from '../../components/ArticleBody';
 import { ProseCard } from '../proseCard';
 import { fragmentManager } from '@/services/FragmentManager';
 import { BlockContainer, backgroundColorMap } from './_shared';
+import { cn } from '@/utils/shadcn';
 
 fragmentManager.register(/* GraphQL */ `
   fragment FeatureBlockDetails on FeatureBlock {
@@ -60,6 +61,7 @@ type Props = {
 
 export function FeatureBlock({ resource }: Props) {
   const backgroundColor = backgroundColorMap[resource?.backgroundColour ?? 'white'];
+  const maxPerRow = resource.maxPerRow ?? 4;
 
   return (
     <BlockContainer className={backgroundColor}>
@@ -73,7 +75,13 @@ export function FeatureBlock({ resource }: Props) {
           <ArticleBody dangerouslySetInnerHTML={{ __html: resource.body }} className="mt-2" />
         </ArticleTextContainer>
       )}
-      <div className="max-w-6xl m-auto p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div
+        className={cn('max-w-6xl m-auto p-10 grid grid-cols-1 gap-5', {
+          'sm:grid-cols-2': maxPerRow >= 2,
+          'lg:grid-cols-3': maxPerRow >= 3,
+          'xl:grid-cols-4': maxPerRow >= 4,
+        })}
+      >
         {resource.features?.map((feature, index) => {
           let image: ProseCardImgFragment | null | undefined;
           if ('optionalImg' in feature) image = feature.optionalImg;
