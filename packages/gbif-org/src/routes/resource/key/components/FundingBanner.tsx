d8e4ea0265136a84b803def0a3ef4b47ede71693
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-03-19 15:27:30 +0100

    funcing banner: close https://github.com/gbif/gbif-web/issues/524

--- packages/gbif-org/src/routes/resource/key/components/FundingBanner.tsx
+++ packages/gbif-org/src/routes/resource/key/components/FundingBanner.tsx
@@ -54,24 +54,42 @@ export function FundingBanner({ resource }: Props) {
 
   return (
     <div className="bg-slate-100 mt-6 p-6 flex flex-col items-center">
-      {fundsAllocated && <span className="text-gray-500 text-xs">
-        € <FormattedNumber value={fundsAllocated} />
-      </span>}
+      {fundsAllocated && (
+        <span className="text-gray-500 text-xs">
+          € <FormattedNumber value={fundsAllocated} />{' '}
+          <FormattedMessage id="cms.project.fundedBy" />
+        </span>
+      )}
 
-      <div className="pt-2 flex gap-8 flex-wrap justify-center">
-        {fundingOrganisations?.filter(notNull).map((fundingOrganisation) => (
-          <div key={fundingOrganisation.id}>
-            <a href={fundingOrganisation.url!} className="flex flex-col items-center group">
-              <img
-                className="max-w-28"
-                src={fundingOrganisation.logo?.file.url}
-              />
-              <span className="text-gray-500 text-sm pt-2">
-                <FormattedMessage id="cms.project.fundedBy" /> : <span className="group-hover:underline">{fundingOrganisation.title}</span>
-              </span>
-            </a>
-          </div>
-        ))}
+      <div className="mt-4 flex gap-8 flex-wrap justify-center">
+        {fundingOrganisations
+          ?.filter(notNull)
+          .filter((x) => !x.logo)
+          .map((fundingOrganisation) => (
+            <div key={fundingOrganisation.id}>
+              <a href={fundingOrganisation.url!} className="flex flex-col items-center group">
+                <span className="text-gray-500 text-sm pt-2 group-hover:underline">
+                  {fundingOrganisation.title}
+                </span>
+              </a>
+            </div>
+          ))}
+      </div>
+
+      <div className="mt-4 flex gap-8 flex-wrap justify-center">
+        {fundingOrganisations
+          ?.filter(notNull)
+          .filter((x) => x.logo)
+          .map((fundingOrganisation) => (
+            <div key={fundingOrganisation.id}>
+              <a href={fundingOrganisation.url!} className="flex flex-col items-center group">
+                <img className="max-w-28" src={fundingOrganisation.logo?.file.url} />
+                <span className="text-gray-500 text-sm pt-2 group-hover:underline">
+                  {fundingOrganisation.title}
+                </span>
+              </a>
+            </div>
+          ))}
       </div>
     </div>
   );
