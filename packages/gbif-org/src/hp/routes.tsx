d0046bf83343e66651e101b783084ce86ea5ac49
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-03-27 09:34:21 +0100

    fix hp paths

--- packages/gbif-org/src/hp/routes.tsx
+++ packages/gbif-org/src/hp/routes.tsx
@@ -11,13 +11,13 @@ import {
 import { OccurrenceSearchPage } from '@/routes/occurrence/search/occurrenceSearchPage';
 import { InputConfig, configBuilder } from '@/contexts/config/config';
 import { DatasetPage, datasetLoader } from '@/routes/dataset/key/datasetKey';
-import { DatasetAboutTab } from '@/routes/dataset/key/about';
-import { DatasetDashboardTab } from '@/routes/dataset/key/dashboard';
-import { DatasetOccurrencesTab } from '@/routes/dataset/key/occurrences';
-import { DatasetDownloadTab } from '@/routes/dataset/key/download';
+import { DatasetKeyAbout } from '@/routes/dataset/key/about';
+import { DatasetKeyDashboard } from '@/routes/dataset/key/dashboard';
+import { DatasetKeyOccurrences } from '@/routes/dataset/key/occurrences';
+import { DatasetKeyDownload } from '@/routes/dataset/key/download';
 import { PublisherPage, publisherLoader } from '@/routes/publisher/key/publisherKey';
-import { PublisherAboutTab } from '@/routes/publisher/key/about';
-import { PublisherOccurrencesTab } from '@/routes/publisher/key/metrics';
+import { PublisherKeyAbout } from '@/routes/publisher/key/about';
+import { PublisherKeyMetrics } from '@/routes/publisher/key/metrics';
 import { NewsPage, newsPageLoader } from '@/routes/resource/key/news/news';
 
 const baseRoutes: SourceRouteObject[] = [
@@ -52,19 +52,19 @@ const baseRoutes: SourceRouteObject[] = [
         children: [
           {
             index: true,
-            element: <DatasetAboutTab />,
+            element: <DatasetKeyAbout />,
           },
           {
             path: 'dashboard',
-            element: <DatasetDashboardTab />,
+            element: <DatasetKeyDashboard />,
           },
           {
             path: 'occurrences',
-            element: <DatasetOccurrencesTab />,
+            element: <DatasetKeyOccurrences />,
           },
           {
             path: 'download',
-            element: <DatasetDownloadTab />,
+            element: <DatasetKeyDownload />,
           },
         ],
       },
@@ -80,11 +80,11 @@ const baseRoutes: SourceRouteObject[] = [
         children: [
           {
             index: true,
-            element: <PublisherAboutTab />,
+            element: <PublisherKeyAbout />,
           },
           {
             path: 'occurrences',
-            element: <PublisherOccurrencesTab />,
+            element: <PublisherKeyMetrics />,
           },
         ],
       },
