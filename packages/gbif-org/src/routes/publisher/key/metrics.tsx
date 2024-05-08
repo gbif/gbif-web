30392509b65fc35dd8fb7ee0cefffe0c1123443c
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-05-08 14:08:40 +0200

    wip publihser page: comments

--- packages/gbif-org/src/routes/publisher/key/metrics.tsx
+++ packages/gbif-org/src/routes/publisher/key/metrics.tsx
@@ -1,5 +1,5 @@
 import { ArticleContainer } from "@/routes/resource/key/components/articleContainer";
 
 export function PublisherKeyMetrics() {
-  return <ArticleContainer className="bg-slate-100 pt-0">Metrics</ArticleContainer>;
+  return <ArticleContainer className="bg-slate-100 pt-0">Will be added once we have implemented occurrence dashboards</ArticleContainer>;
 }
