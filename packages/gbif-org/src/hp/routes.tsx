5c0aab348e077b41155c419b6402dae3c1558eec
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-06-11 11:47:02 +0200

    update hp routes

--- packages/gbif-org/src/hp/routes.tsx
+++ packages/gbif-org/src/hp/routes.tsx
@@ -10,118 +10,316 @@ import {
 } from '@/routes/occurrence/key/occurrenceKey';
 import { OccurrenceSearchPage } from '@/routes/occurrence/search/occurrenceSearchPage';
 import { InputConfig, configBuilder } from '@/contexts/config/config';
-import { DatasetPage, datasetLoader } from '@/routes/dataset/key/datasetKey';
+import { DatasetPage, DatasetPageSkeleton, datasetLoader } from '@/routes/dataset/key/datasetKey';
 import { DatasetKeyAbout } from '@/routes/dataset/key/about';
 import { DatasetKeyDashboard } from '@/routes/dataset/key/dashboard';
 import { DatasetKeyOccurrences } from '@/routes/dataset/key/occurrences';
 import { DatasetKeyDownload } from '@/routes/dataset/key/download';
-import { PublisherPage, publisherLoader } from '@/routes/publisher/key/publisherKey';
+import { PublisherPage, PublisherPageSkeleton, publisherLoader } from '@/routes/publisher/key/publisherKey';
 import { PublisherKeyAbout } from '@/routes/publisher/key/about';
 import { PublisherKeyMetrics } from '@/routes/publisher/key/metrics';
-import { NewsPage, newsPageLoader } from '@/routes/resource/key/news/news';
+import { NewsPage, NewsPageSkeleton, newsPageLoader } from '@/routes/resource/key/news/news';
 import { InstitutionKey, InstitutionKeyAbout, InstitutionKeyCollection, InstitutionKeySpecimens, institutionLoader } from '@/routes/institution/key';
+import { RouteId } from '@/hooks/useParentRouteLoaderData';
+import { OccurrenceKeyAbout } from '@/routes/occurrence/key/about';
+import { OccurrenceKeyPhylo } from '@/routes/occurrence/key/phylogenies';
+import { OccurrenceKeyCluster } from '@/routes/occurrence/key/cluster';
+import { PublisherKeyCitations } from '@/routes/publisher/key/citations';
+import { InstallationPage, InstallationPageSkeleton, installationLoader } from '@/routes/installation/key/installationKey';
+import { InstallationKeyAbout } from '@/routes/installation/key/about';
+import { NetworkPage, NetworkPageSkeleton, networkLoader } from '@/routes/network/key/networkKey';
+import { NetworkKeyAbout } from '@/routes/network/key/about';
+import { NetworkKeyMetrics } from '@/routes/network/key/metrics';
+import { NetworkKeyDataset } from '@/routes/network/key/dataset';
+import { NetworkKeyPublisher } from '@/routes/network/key/publisher';
+import { CollectionKey, CollectionKeyAbout, CollectionKeyDashboard, CollectionKeySpecimens, collectionLoader } from '@/routes/collection/key';
+import { EventPage, EventPageSkeleton, eventPageLoader } from '@/routes/resource/key/event/event';
+import { ToolPage, ToolPageSkeleton, toolPageLoader } from '@/routes/resource/key/tool/tool';
+import { DataUsePage, DataUsePageSkeleton, dataUsePageLoader } from '@/routes/resource/key/dataUse/dataUse';
+import { ArticlePage, ArticlePageSkeleton, articlePageLoader } from '@/routes/resource/key/article/article';
+import { DocumentPage, DocumentPageSkeleton, documentPageLoader } from '@/routes/resource/key/document/document';
+import { ProgrammePage, ProgrammePageSkeleton, programmePageLoader } from '@/routes/resource/key/programme/programme';
+import { CompositionPage, CompositionPageSkeleton, compositionPageLoader } from '@/routes/resource/key/composition/composition';
+import { ProjectAboutTab, ProjectDatasetsTab, ProjectNewsAndEventsTab, ProjectNewsAndEventsTabSkeleton, ProjectPage, ProjectPageSkeleton, projectNewsAndEventsLoader, projectPageLoader } from '@/routes/resource/key/project';
+import { AliasHandling, AliasHandlingSkeleton, aliasHandlingLoader } from '@/routes/resource/key/aliasHandling';
 
 const baseRoutes: SourceRouteObject[] = [
   {
     errorElement: <RootErrorPage />,
     children: [
       {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        key: 'occurrence-search-page',
-        path: 'occurrence/search',
-        element: <OccurrenceSearchPage />,
-      },
-      {
-        key: 'occurrence-page',
-        path: 'occurrence/:key',
-        loader: occurrenceKeyLoader,
-        element: <OccurrenceKey />,
-        loadingElement: <OccurrenceKeySkeleton />,
-      },
-      {
-        key: 'dataset-page',
-        gbifRedirect: (params) => {
-          if (typeof params.key !== 'string') throw new Error('Invalid key');
-          return `https://www.gbif.org/dataset/${params.key}`;
-        },
-        path: 'dataset/:key',
-        loader: datasetLoader,
-        element: <DatasetPage />,
+        errorElement: <RootErrorPage />,
         children: [
           {
             index: true,
-            element: <DatasetKeyAbout />,
+            element: <HomePage />,
           },
           {
-            path: 'dashboard',
-            element: <DatasetKeyDashboard />,
+            key: 'occurrence-search-page',
+            path: 'occurrence/search',
+            element: <OccurrenceSearchPage />,
           },
           {
-            path: 'occurrences',
-            element: <DatasetKeyOccurrences />,
+            id: RouteId.Occurrence,
+            path: 'occurrence/:key',
+            loader: occurrenceKeyLoader,
+            loadingElement: <OccurrenceKeySkeleton />,
+            element: <OccurrenceKey />,
+            children: [
+              {
+                index: true,
+                element: <OccurrenceKeyAbout />,
+              },
+              {
+                path: 'phylogenies',
+                element: <OccurrenceKeyPhylo />,
+              },
+              {
+                path: 'related',
+                element: <OccurrenceKeyCluster />,
+              }
+            ]
           },
           {
-            path: 'download',
-            element: <DatasetKeyDownload />,
+            id: RouteId.Dataset,
+            gbifRedirect: (params) => {
+              if (typeof params.key !== 'string') throw new Error('Invalid key');
+              return `https://www.gbif.org/dataset/${params.key}`;
+            },
+            path: 'dataset/:key',
+            loader: datasetLoader,
+            loadingElement: <DatasetPageSkeleton />,
+            element: <DatasetPage />,
+            children: [
+              {
+                index: true,
+                element: <DatasetKeyAbout />,
+              },
+              {
+                path: 'dashboard',
+                element: <DatasetKeyDashboard />,
+              },
+              {
+                path: 'occurrences',
+                element: <DatasetKeyOccurrences />,
+              },
+              {
+                path: 'download',
+                element: <DatasetKeyDownload />,
+              },
+            ],
+          },
+          {
+            id: RouteId.Publisher,
+            gbifRedirect: (params) => {
+              if (typeof params.key !== 'string') throw new Error('Invalid key');
+              return `https://www.gbif.org/publisher/${params.key}`;
+            },
+            path: 'publisher/:key',
+            loader: publisherLoader,
+            loadingElement: <PublisherPageSkeleton />,
+            element: <PublisherPage />,
+            children: [
+              {
+                index: true,
+                element: <PublisherKeyAbout />,
+              },
+              {
+                path: 'metrics',
+                element: <PublisherKeyMetrics />,
+              },
+              {
+                path: 'citations',
+                element: <PublisherKeyCitations />,
+              }
+            ],
+          },
+          {
+            id: RouteId.Installation,
+            gbifRedirect: (params) => {
+              if (typeof params.key !== 'string') throw new Error('Invalid key');
+              return `https://www.gbif.org/installation/${params.key}`;
+            },
+            path: 'installation/:key',
+            loader: installationLoader,
+            loadingElement: <InstallationPageSkeleton />,
+            element: <InstallationPage />,
+            children: [
+              {
+                index: true,
+                element: <InstallationKeyAbout />,
+              }
+            ],
+          },
+          {
+            id: RouteId.Network,
+            gbifRedirect: (params) => {
+              if (typeof params.key !== 'string') throw new Error('Invalid key');
+              return `https://www.gbif.org/network/${params.key}`;
+            },
+            path: 'network/:key',
+            loader: networkLoader,
+            loadingElement: <NetworkPageSkeleton />,
+            element: <NetworkPage />,
+            children: [
+              {
+                index: true,
+                element: <NetworkKeyAbout />,
+              },
+              {
+                path: 'metrics',
+                element: <NetworkKeyMetrics />,
+              },
+              {
+                path: 'dataset',
+                element: <NetworkKeyDataset />,
+              },
+              {
+                path: 'publisher',
+                element: <NetworkKeyPublisher />,
+              },
+            ],
+          },
+          {
+            id: RouteId.Institution, // TODO @daniel is there a reason for key vs id?
+            gbifRedirect: (params) => {
+              if (typeof params.key !== 'string') throw new Error('Invalid key');
+              return `https://www.gbif.org/institution/${params.key}`;
+            },
+            path: 'institution/:key',
+            loader: institutionLoader,
+            element: <InstitutionKey />,
+            children: [
+              {
+                index: true,
+                element: <InstitutionKeyAbout />,
+              },
+              {
+                path: 'specimen',
+                element: <InstitutionKeySpecimens />,
+              },
+              {
+                path: 'collection',
+                element: <InstitutionKeyCollection />,
+              },
+            ],
+          },
+          {
+            id: RouteId.Collection,
+            gbifRedirect: (params) => {
+              if (typeof params.key !== 'string') throw new Error('Invalid key');
+              return `https://www.gbif.org/collection/${params.key}`;
+            },
+            path: 'collection/:key',
+            loader: collectionLoader,
+            element: <CollectionKey />,
+            children: [
+              {
+                index: true,
+                element: <CollectionKeyAbout />,
+              },
+              {
+                path: 'specimen',
+                element: <CollectionKeySpecimens />,
+              },
+              {
+                path: 'dashboard',
+                element: <CollectionKeyDashboard />,
+              },
+            ],
+          },
+          {
+            path: 'news/:key',
+            loader: newsPageLoader,
+            loadingElement: <NewsPageSkeleton />,
+            element: <NewsPage />,
+            isSlugified: true,
+          },
+          {
+            path: 'event/:key',
+            loader: eventPageLoader,
+            loadingElement: <EventPageSkeleton />,
+            element: <EventPage />,
+            isSlugified: true,
+          },
+          {
+            path: 'tool/:key',
+            loader: toolPageLoader,
+            loadingElement: <ToolPageSkeleton />,
+            element: <ToolPage />,
+            isSlugified: true,
+          },
+          {
+            path: 'data-use/:key',
+            loader: dataUsePageLoader,
+            loadingElement: <DataUsePageSkeleton />,
+            element: <DataUsePage />,
+            isSlugified: true,
+          },
+          {
+            path: 'article/:key',
+            loader: articlePageLoader,
+            loadingElement: <ArticlePageSkeleton />,
+            element: <ArticlePage />,
+            isSlugified: true,
+          },
+          {
+            path: 'document/:key',
+            loader: documentPageLoader,
+            loadingElement: <DocumentPageSkeleton />,
+            element: <DocumentPage />,
+            isSlugified: true,
+          },
+          {
+            path: 'programme/:key',
+            loader: programmePageLoader,
+            loadingElement: <ProgrammePageSkeleton />,
+            element: <ProgrammePage />,
+            isSlugified: true,
+          },
+          {
+            path: 'composition/:key',
+            loader: compositionPageLoader,
+            loadingElement: <CompositionPageSkeleton />,
+            element: <CompositionPage />,
+            isSlugified: true,
+          },
+          {
+            id: RouteId.Project,
+            path: 'project/:key',
+            loader: projectPageLoader,
+            loadingElement: <ProjectPageSkeleton />,
+            element: <ProjectPage />,
+            isSlugified: true,
+            children: [
+              {
+                index: true,
+                element: <ProjectAboutTab />,
+              },
+              {
+                path: 'news',
+                element: <ProjectNewsAndEventsTab />,
+                loadingElement: <ProjectNewsAndEventsTabSkeleton />,
+                loader: projectNewsAndEventsLoader,
+              },
+              {
+                path: 'datasets',
+                element: <ProjectDatasetsTab />,
+              },
+            ],
+          },
+          {
+            path: ':alias',
+            loader: aliasHandlingLoader,
+            loadingElement: <AliasHandlingSkeleton />,
+            element: <AliasHandling />,
+          },
+          {
+            path: '*',
+            // Delegate handling of 404 to RootErrorPage,
+            element: <ThrowOn404 />,
           },
         ],
-      },
-      {
-        key: 'publisher-page',
-        gbifRedirect: (params) => {
-          if (typeof params.key !== 'string') throw new Error('Invalid key');
-          return `https://www.gbif.org/publisher/${params.key}`;
-        },
-        path: 'publisher/:key',
-        loader: publisherLoader,
-        element: <PublisherPage />,
-        children: [
-          {
-            index: true,
-            element: <PublisherKeyAbout />,
-          },
-          {
-            path: 'occurrences',
-            element: <PublisherKeyMetrics />,
-          },
-        ],
-      },
-      {
-        key: 'institution-page',
-        gbifRedirect: (params) => {
-          if (typeof params.key !== 'string') throw new Error('Invalid key');
-          return `https://www.gbif.org/institution/${params.key}`;
-        },
-        path: 'institution',
-        loader: institutionLoader,
-        element: <InstitutionKey />,
-        children: [
-          {
-            index: true,
-            element: <InstitutionKeyAbout />,
-          },
-          {
-            path: 'specimen',
-            element: <InstitutionKeySpecimens />,
-          },
-          {
-            path: 'collection',
-            element: <InstitutionKeyCollection />,
-          },
-        ],
-      },
-      {
-        path: 'resource/:key',
-        loader: newsPageLoader,
-        element: <NewsPage />,
-      },
-      {
-        path: '*',
-        // Delegate handling of 404 to RootErrorPage,
-        element: <ThrowOn404 />,
       },
     ],
   },
