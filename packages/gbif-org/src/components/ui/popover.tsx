a68b8b0c981696970c1111be8f1d9cfcc608648a
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-06-20 15:21:34 +0200

    fix: popover should be wrappde in gbif class to work with tailwind

--- packages/gbif-org/src/components/ui/popover.tsx
+++ packages/gbif-org/src/components/ui/popover.tsx
@@ -12,16 +12,18 @@ const PopoverContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Portal>
+    <div className="gbif">
     <PopoverPrimitive.Content
       ref={ref}
       align={align}
       sideOffset={sideOffset}
       style={{maxWidth: 'calc(100vw'}}
-      className={cn('g-z-50 g-rounded-md g-border g-bg-popover g-p-4 text-popover-foreground g-shadow-md g-outline-none data-[state=open]:g-animate-in data-[state=closed]:g-animate-out data-[state=closed]:g-fade-out-0 data-[state=open]:g-fade-in-0 data-[state=closed]:g-zoom-out-95 data-[state=open]:g-zoom-in-95 data-[side=bottom]:g-slide-in-from-top-2 data-[side=left]:g-slide-in-from-right-2 data-[side=right]:g-slide-in-from-left-2 data-[side=top]:g-slide-in-from-bottom-2',
+      className={cn('g-z-50 g-rounded-md g-border g-bg-popover g-p-4 g-text-popover-foreground g-shadow-md g-outline-none data-[state=open]:g-animate-in data-[state=closed]:g-animate-out data-[state=closed]:g-fade-out-0 data-[state=open]:g-fade-in-0 data-[state=closed]:g-zoom-out-95 data-[state=open]:g-zoom-in-95 data-[side=bottom]:g-slide-in-from-top-2 data-[side=left]:g-slide-in-from-right-2 data-[side=right]:g-slide-in-from-left-2 data-[side=top]:g-slide-in-from-bottom-2',
         className
       )}
       {...props}
     />
+    </div>
   </PopoverPrimitive.Portal>
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
