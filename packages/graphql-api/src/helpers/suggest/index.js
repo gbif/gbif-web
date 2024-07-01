f53e1f1828a862fffd2a63fb88d98b1ba4e9499c
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-07-01 13:43:18 +0200

    wip: cross filter suggest

--- packages/graphql-api/src/helpers/suggest/index.js
+++ packages/graphql-api/src/helpers/suggest/index.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import config from '../../config';
 import { matchSorter } from 'match-sorter'
+import colSuggest from '#/resources/gbif/taxon/colSuggest';
 
 const translationEndpoint = config.translations || 'https://react-components.gbif.org/lib/translations';
 // local cache for translations
@@ -81,7 +82,8 @@ const filters = [
 export async function getFieldNames(lang) {
   const translations = await getTranslations(lang);
   if (!translations) {
-    return filters;
+    console.error('No translations found');
+    return [];
   }
   const filterNames = filters.map(f => ({ filter: f, label: translations[`filters.${f}.name`] || f })).sort();
   // map to format for filters [{filter: 'country', label: 'Denmark', alternativeLabels: []}]
@@ -89,10 +91,52 @@ export async function getFieldNames(lang) {
     return {
       filter: f.filter,
       label: f.label,
-      alternativeLabels: [],
+      alsoKnownAs: [f.filter],
+      alternativeLabels: [f.filter],
       type: 'FILTER'
     }
   });
+}
+async function getYearSuggestions({ q } = {}) {
+  // if it looks like a single year or a yearspan, then return a suggestion, else return nothing
+  // years should be between 1500-20
+  const maxYear = new Date().getFullYear();
+  const minYear = 1500;
+
+  // parse q as number
+  const parts = q.split(/[,\-]/);
+  const [start, end] = parts.map(x => {
+    const v = x.trim();
+    if (v === '' || v === '*') return '*' // empty string is a wildcard
+    return parseInt(v);
+  });
+  
+  // if there is only a start and it isn't a number, then return nothing
+  if (isNaN(start) && !end) {
+    return [];
+  }
+  // if it is a range, then compose the range
+  if (typeof start !== 'undefined' && typeof end !== 'undefined') {
+    if (start !== '*' && (start < minYear || start > maxYear)) {
+      return [];
+    }
+    if (end !== '*' && (end < minYear || end > maxYear)) {
+      return [];
+    } else if (end !== '*' && start !== '*' && start >= end) {
+      // it would be nice to suggest a valid range to the user that still respects what they have entered. 
+      // E.g. if they enter 1800,18 then suggest 1800-1899 or such. 1930-193 => 1930-1939 , 1930,194 => 1930-1949
+      return [];
+    } else if (end === '*' && start === '*') {
+      return [];
+    }
+    // The range should be within 1500- this year + 10
+    return [{ filter: 'year', value: `${start},${end}`, label: `${start}-${end}`, alternativeLabels: [] }];
+  }
+  // if it is a number, then check if it is within the range
+  if (start > minYear && start <= maxYear) {
+    return [{ filter: 'year', value: start, label: `${start}`, alternativeLabels: [] }];
+  }
+  return [];
 }
 
 async function getCountryNames(lang) {
@@ -115,7 +159,7 @@ async function getCountryNames(lang) {
   return countryNames;
 }
 
-async function getDatasetSuggestions({ q, limit = 1 }) {
+async function getDatasetSuggestions({ q, limit = 2 }) {
   if (q.length < 3) return [];
   const apiUrl = `${config.apiv1}/dataset/suggest?q=${q}&limit=${limit}`;
   try {
@@ -143,9 +187,16 @@ async function getDatasets({ q }) {
 export async function getSpeciesSuggestions({ q, lang, taxonKeys }) {
   // use custom suggest via graphql
   const langMap = {
-    en: 'eng'
+    en: 'eng',
+    da: 'dan',
+    es: 'spa',
+    de: 'deu'
   }
   const knownLang = langMap[lang] ?? 'en';
+
+  const result = await colSuggest({ language: knownLang, q, taxonKeys });
+  return result;
+
   const queryString = `
   query{
     taxonSuggestions( q: "${q}", language: ${knownLang}) {
@@ -220,32 +271,32 @@ export async function getSpecies({ q, taxonKeys, lang }) {
       filter: 'taxonKey',
       value: s.key,
       label: s.scientificName,
-      alsoKnownAs: [s.vernacularName, s.acceptedNameOf].filter(x => x),
+      alsoKnownAs: [s.vernacularName, s.acceptedNameOf, s.canonicalName].filter(x => x),
       alternativeLabels: ['taxon', 'species', 'sp', s.vernacularName, s.acceptedNameOf].filter(x => x),
       item: s
     };
-  }).slice(0,5);
+  }).slice(0, 5);
 }
 
-async function getGadmSuggestions({q, gadmId, limit = 2}) {
+async function getGadmSuggestions({ q, gadmId, limit = 2 }) {
   // https://api.gbif.org/v1/geocode/gadm/search?limit=100&q=k%C3%B8benhavn
   const apiUrl = `${config.apiv1}/geocode/gadm/search?limit=100&q=${q}`;
   try {
     const response = await axios.get(apiUrl);
     // map to format for filters [{filter: 'gadmGid', value: 'DK.02', label: 'København', alternativeLabels: []}]
     return response.data.results.
-    filter(x => {
-      // remove gadm level 0
-      return x.gadmLevel > 0;
-    }).map(s => {
-      return {
-        filter: 'gadmGid',
-        value: s.id,
-        label: s.name,
-        alternativeLabels: ['gadm']
-      };
-    }
-    ).slice(0, limit);
+      filter(x => {
+        // remove gadm level 0
+        return x.gadmLevel > 0;
+      }).map(s => {
+        return {
+          filter: 'gadmGid',
+          value: s.id,
+          label: s.name,
+          alternativeLabels: ['gadm']
+        };
+      }
+      ).slice(0, limit);
   } catch (error) {
     console.log(error);
     return [];
@@ -259,24 +310,29 @@ async function getCandidates({ lang = 'en', query, taxonKeys }) {
   // get datasets, countries, species and field names
   // and concatenate them into a single array
   // fetch them all at the same time
-  const [datasets, countries, species, fields, gadm] = await Promise.all([
+  const [datasets, countries, species, fields, gadm, years] = await Promise.all([
     getDatasets({ q: query }),
     getCountryNames(lang),
     getSpecies({ q: query, taxonKeys, lang }),
     getFieldNames(lang),
-    getGadmSuggestions({ q: query })
+    getGadmSuggestions({ q: query }),
+    getYearSuggestions({ q: query }),
   ]);
-  return filterSuggestions({ list: datasets, q: query, threshold: matchSorter.rankings.WORD_STARTS_WITH })
+  return datasets
     .concat(filterSuggestions({ list: countries, q: query, threshold: matchSorter.rankings.WORD_STARTS_WITH }))
-    .concat(filterSuggestions({ list: species, q: query, threshold: matchSorter.rankings.STARTS_WITH }))
+    .concat(species)
     .concat(filterSuggestions({ list: fields, q: query, threshold: matchSorter.rankings.WORD_STARTS_WITH }))
-    .concat(filterSuggestions({ list: gadm, q: query, threshold: matchSorter.rankings.WORD_STARTS_WITH }));
+    .concat(gadm)
+    .concat(years);
 }
 
 function filterSuggestions({ list, q, threshold }) {
   if (!q) return list;
   // add indeces to the list
   list.forEach((item, index) => {
+    if (typeof item !== 'object') {
+      return;
+    }
     item.index = index;
   });
   // if spaces, then split and search for each word
@@ -306,25 +362,32 @@ export async function getSuggestions({ lang, q, taxonKeys }) {
   const results = [];
   parts.forEach(part => {
     const sorted = matchSorter(candidates, part, {
-      // threshold: matchSorter.rankings.STARTS_WITH,
+      threshold: matchSorter.rankings.NO_MATCH,
       keys: ['label', { maxRanking: matchSorter.rankings.CASE_SENSITIVE_EQUAL, key: 'alsoKnownAs' }, { maxRanking: matchSorter.rankings.CONTAINS, key: 'alternativeLabels' }],
       // use index to sort the results
       baseSort: (a, b) => {
+        //lineas 2346677
+        // lam: 7351755
         // some filters should go before others. The ordering should be field, taxonKey, country, other
         // field is of type=FILTER, the other has filter=taxonKey etc
         function decorate(x) {
-          let v = x.index ?? 0;
-          if (x.type === 'FILTER') v += 10000;
+          let v = x.index / 100 ?? 0;
+          const item = x.item;
+          if (item.type === 'FILTER') v += 10000;
           const boosts = {
-            taxonKey: 1000,
-            country: 900,
-            continent: 750,
-            year: 800,
-            gadm: 700,
+            year: 1000,
+            taxonKey: 900,
+            country: 800,
+            continent: 700,
+            gadmGid: 600,
           }
-          if (x.filter && boosts[x.filter]) {
-            v += boosts[x.filter];
+          if (item.filter && boosts[item.filter]) {
+            v += boosts[item.filter];
           }
+          if (item.filter === 'taxonKey' && item?.item?.taxon?.status === 'accepted') {
+            v += 10000;
+          }
+          return v;
         }
         const aVal = decorate(a);
         const bVal = decorate(b);
@@ -332,7 +395,17 @@ export async function getSuggestions({ lang, q, taxonKeys }) {
         return aVal > bVal ? -1 : 1;
       }
     });
-    results.push(sorted);
+    // move all dataset results to the bottom
+    const datasetResults = sorted.filter(x => x.filter === 'datasetKey');
+    const otherResults = sorted.filter(x => x.filter !== 'datasetKey');
+    const combined = otherResults.concat(datasetResults);
+
+    // always show filters on top
+    const filters = combined.filter(x => x.type === 'FILTER');
+    const others = combined.filter(x => x.type !== 'FILTER');
+    const filterSorted = filters.concat(others);
+
+    results.push(filterSorted);
   });
   // if there are multiple parts, then return the intersection of the results
   const intersection = results.reduce((acc, val) => acc.filter(x => val.includes(x)));
