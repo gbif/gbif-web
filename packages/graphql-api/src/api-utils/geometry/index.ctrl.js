b95e5d50fc51bdde08a1d94c832d06d5fc22daed
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-07-29 10:10:48 +0200

    feat: wip geometry simplifier (annotations)

---
+++ packages/graphql-api/src/api-utils/geometry/index.ctrl.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import axios from 'axios';
+import { stringify } from 'wkt';
+import mapshaper from 'mapshaper';
+
+const router = Router();
+
+export default (app) => {
+  app.use('/unstable-api/geometry', router);
+};
+// GBIF Natural
+router.get('/simplify/gadm/:level/:id', async (req, res, next) => {
+  try {
+    const response = await axios.get(`https://api.gbif-uat.org/v1/geocode/feature/gadm${req.params.level}.json?id=${req.params.id}`);
+    let geojson = response.data;
+
+    const input = {
+      'input.geojson': JSON.stringify(geojson),
+      percentage: 0.1
+    };
+    const cmd = '-i input.geojson -simplify resolution=100x100 -o output.geojson';
+
+    // using Promise
+    const output = await mapshaper.applyCommands(cmd, input);
+    var simplified = JSON.parse(output['output.geojson'].toString());
+
+    if (req.query.format === 'WKT') {
+      const wktString = stringify(simplified.features[0].geometry);
+      res.json({ wkt: wktString });
+    } else {
+      res.json(simplified);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
--- packages/graphql-api/src/index.ts
+++ packages/graphql-api/src/index.ts
@@ -29,6 +29,7 @@ import ipController from './api-utils/ip2country.ctrl.js';
 import polygonName from './api-utils/polygonName.ctrl.js';
 import suggestFilter from './api-utils/suggestFilter.ctrl.js';
 import formController from './api-utils/forms/index.ctrl';
+import geometryController from './api-utils/geometry/index.ctrl.js';
 import { loggingPlugin } from './plugins/loggingPlugin';
 
 // we are doing this async as we need to load the various enumerations from the APIs
@@ -111,12 +112,12 @@ async function initializeServer() {
 
   app.get('/health', health);
 
-  // utils for map styles
   mapController(app);
   ipController(app);
   polygonName(app);
   formController(app);
   suggestFilter(app);
+  geometryController(app);
 
   await server.start();
   server.applyMiddleware({ app });
