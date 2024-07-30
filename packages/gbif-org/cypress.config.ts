5b39954ccfcd62c976903e1ad73cf742f7454932
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2024-07-30 10:04:43 +0200

    Sauce labs config

--- packages/gbif-org/cypress.config.ts
+++ packages/gbif-org/cypress.config.ts
@@ -5,7 +5,7 @@ const env = loadEnv('', process.cwd(), ['PUBLIC_']);
 
 export default defineConfig({
   e2e: {
-    baseUrl: env.PUBLIC_BASE_URL,
+    baseUrl: env.TEST_BASE_URL ?? env.PUBLIC_BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
