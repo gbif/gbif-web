96d04b0229455ba04087340aa6cae6982169dce3
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-07-31 14:49:26 +0200

    wip

--- packages/react-components/src/entities/Collection/about/DescriptorGroups.js
+++ packages/react-components/src/entities/Collection/about/DescriptorGroups.js
@@ -1,8 +1,12 @@
+import { css, jsx } from '@emotion/react';
 import React, { useContext, useState, useEffect } from 'react';
 import { useQuery } from '../../../dataManagement/api';
 import { Card } from "../../shared";
+// import { BiSpreadsheet as SpreadSheetIcon } from "react-icons/bi";
+import { RiFileDownloadLine as SpreadSheetIcon } from "react-icons/ri";
+import { DataTable, Th, Td, TBody } from '../../../components';
 
-export function DescriptorGroups({collectionKey}) {
+export function DescriptorGroups({ collectionKey }) {
   const { data, error, loading, load } = useQuery(DESCRIPTOR_GROUPS, { lazyLoad: true });
 
   useEffect(() => {
@@ -23,7 +27,7 @@ export function DescriptorGroups({collectionKey}) {
   return data.collection.descriptorGroups.results.map(group => <DescriptorGroupPresentation key={group.key} collectionKey={collectionKey} groupKey={group.key} {...group} />);
 }
 
-function DescriptorGroupPresentation({collectionKey, groupKey, title, description}) {
+function DescriptorGroupPresentation({ collectionKey, groupKey, title, description }) {
   const { data, error, loading, load } = useQuery(DESCRIPTOR_GROUP, { lazyLoad: true });
 
   useEffect(() => {
@@ -38,17 +42,74 @@ function DescriptorGroupPresentation({collectionKey, groupKey, title, descriptio
     }
   }, [collectionKey, groupKey]);
 
-  return <Card style={{ marginTop: 0, marginBottom: 24 }}>
-    <h3>{title}</h3>
-    <div>
-      {description}
-    </div>
-    <div>
-      {loading && 'Loading...'}
-      {error && 'Error'}
-      {data && data.collectionDescriptorGroup && data.collectionDescriptorGroup.descriptors.results.map(d => <div key={d.key}>
-        {Object.keys(d.verbatim).map(k => <span key={k}>{k}: {d.verbatim[k]}</span>)}
-      </div>)}
+  const hasResults = data?.collectionDescriptorGroup?.descriptors?.results?.length > 0;
+  const keys = hasResults ? Object.keys(data?.collectionDescriptorGroup?.descriptors?.results[0].verbatim) : [];
+
+  return <Card style={{ marginTop: 0, marginBottom: 8, paddingTop: 12, paddingBottom: 12 }}>
+    <div style={{ display: 'flex' }}>
+      <div style={{ marginRight: 24 }}>
+        <a href={`https://api.gbif-uat.org/v1/grscicoll/collection/${collectionKey}/descriptorSet/${groupKey}/export?format=CSV`} style={{ color: 'var(--color800)' }}>
+          <SpreadSheetIcon style={{ fontSize: 32 }} />
+        </a>
+      </div>
+      <div>
+        <h4 style={{ marginTop: 0, marginBottom: 8 }}>{title}</h4>
+        <div style={{ color: 'var(--color600)' }}>
+          {description}
+        </div>
+        <div>
+          {loading && 'Loading...'}
+          {error && 'Error'}
+          {hasResults && <>
+            {/* <table css={css`
+              thead th {
+                font-weight: 400;
+                background: #8a97a0;
+                color: #FFF;
+              }
+
+              tr {
+                background: #f4f7f8;
+                border-bottom: 1px solid #FFF;
+                margin-bottom: 5px;
+              }
+
+              tr:nth-child(even) {
+                background: #e8eeef;
+              }
+
+              th, td {
+                text-align: left;
+                padding: 20px;
+                font-weight: 300;
+              }
+
+              tfoot tr {
+                background: none;
+              }
+
+              tfoot td {
+                padding: 10px 2px;
+                font-size: 0.8em;
+                font-style: italic;
+                color: #8a97a0;
+              }
+              `}> */}
+              <DataTable>
+              <thead>
+                <tr>
+                  {keys.map(key => <Th key={key}>{key}</Th>)}
+                </tr>
+              </thead>
+              <TBody>
+                {data.collectionDescriptorGroup.descriptors.results.map(d => <tr key={d.key}>
+                  {keys.map(k => <Td key={k}>{d.verbatim[k]}</Td>)}
+                </tr>)}
+              </TBody>
+            </DataTable>
+          </>}
+        </div>
+      </div>
     </div>
   </Card>
 }
