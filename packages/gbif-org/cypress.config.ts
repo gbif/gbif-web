bc7ba699daa6b19796cca1498a01c16767967308
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2024-08-19 14:22:11 +0200

    update cypress config

--- packages/gbif-org/cypress.config.ts
+++ packages/gbif-org/cypress.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'cypress';
-import { loadEnv } from 'vite';
-
-const env = loadEnv('', process.cwd(), ['PUBLIC_']);
-// Environment variables takes priority over .env file
-const PUBLIC_TEST_BASE_URL = process.env.PUBLIC_TEST_BASE_URL ?? env.PUBLIC_TEST_BASE_URL
 
 export default defineConfig({
   e2e: {
-    baseUrl: PUBLIC_TEST_BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
