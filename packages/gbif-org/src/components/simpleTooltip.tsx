8c3ae9dd31f45ab0cef99a6052a77bda2976f181
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-10-23 14:14:09 +0200

    feat: more params on tooltip wrapper. Avoid pointer events

--- packages/gbif-org/src/components/simpleTooltip.tsx
+++ packages/gbif-org/src/components/simpleTooltip.tsx
@@ -2,16 +2,19 @@ import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 export function SimpleTooltip({
   title,
+  delayDuration = 0,
   children,
   side,
 }: {
   title: React.ReactNode;
+  delayDuration?: number;
   children: React.ReactNode;
+  side: 'top' | 'right' | 'bottom' | 'left';
 }) {
   return (
-    <Tooltip delayDuration={0}>
-      <TooltipTrigger>{children}</TooltipTrigger>
-      <TooltipContent className='g-max-w-96' side={side}>{title}</TooltipContent>
+    <Tooltip delayDuration={delayDuration}>
+      <TooltipTrigger tabIndex={-1} asChild>{children}</TooltipTrigger>
+      <TooltipContent className='g-max-w-96 g-pointer-events-none' side={side}>{title}</TooltipContent>
     </Tooltip>
   );
 }
