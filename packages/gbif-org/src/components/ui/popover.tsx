f02365dc8f3ea8a33154f797c2309ca37b6fe025
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-10-23 20:52:26 +0200

    styling: popovers

--- packages/gbif-org/src/components/ui/popover.tsx
+++ packages/gbif-org/src/components/ui/popover.tsx
@@ -10,15 +10,21 @@ const PopoverTrigger = PopoverPrimitive.Trigger
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+>(({ className, align = "center", sideOffset = 4, style, ...props }, ref) => (
   <PopoverPrimitive.Portal>
     <div className="gbif">
     <PopoverPrimitive.Content
       ref={ref}
       align={align}
       sideOffset={sideOffset}
-      style={{maxWidth: 'calc(100vw'}}
+      style={{
+        // in theory adding this would make the popover scrollable, but it doesn't work since then we cut of dropdowns within the popover as well
+        // maxHeight: `var(--radix-popover-content-available-height)`,
+        // overflow: "auto",
+        ...style
+      }}
       className={cn('g-z-50 g-rounded-md g-border g-bg-popover g-p-4 g-text-popover-foreground g-shadow-md g-outline-none data-[state=open]:g-animate-in data-[state=closed]:g-animate-out data-[state=closed]:g-fade-out-0 data-[state=open]:g-fade-in-0 data-[state=closed]:g-zoom-out-95 data-[state=open]:g-zoom-in-95 data-[side=bottom]:g-slide-in-from-top-2 data-[side=left]:g-slide-in-from-right-2 data-[side=right]:g-slide-in-from-left-2 data-[side=top]:g-slide-in-from-bottom-2',
+        '[&::-webkit-scrollbar]:g-w-1 [&::-webkit-scrollbar-track]:g-bg-gray-100 [&::-webkit-scrollbar-thumb]:g-bg-gray-300',
         className
       )}
       {...props}
