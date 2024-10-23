5eb6da68ba1434a5cfa7a54bcf2d84563d244f9e
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-10-23 20:50:36 +0200

    feat: make skeletons slightly transparent . close #653

--- packages/gbif-org/src/components/ui/skeleton.tsx
+++ packages/gbif-org/src/components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/utils/shadcn';
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div aria-hidden className={cn('g-animate-pulse g-rounded-md g-bg-slate-200 g-text-transparent [&>*]:g-invisible', className)} {...props} />;
+  return <div aria-hidden className={cn('g-animate-pulse g-rounded-md g-bg-slate-900/10 g-text-transparent [&>*]:g-invisible', className)} {...props} />;
 }
 
 export { Skeleton };
