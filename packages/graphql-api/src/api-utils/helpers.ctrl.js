6712c0300f549e653781cfd9263f7eaf70e6b344
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-11-04 15:07:54 +0100

    feat: wip sql cube endpoint

---
+++ packages/graphql-api/src/api-utils/helpers.ctrl.js
@@ -0,0 +1,32 @@
+/*
+Experimental endpoint to provide a human readable form for WKT polygons.
+The idea is to use our geocoding layers to provide results like: denmark, copenhagen area, gentofte and dragør.
+*/
+import { Router } from 'express';
+import { getSql } from '#/helpers/generateSql';
+
+const router = Router();
+
+export default (app) => {
+  app.use('/unstable-api', router);
+};
+
+router.get('/generate-sql', async (req, res) => {
+  try {
+    // eslint-disable-next-line no-use-before-define
+    const response = await getSql({ query: req.query });
+    res.json(response);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post('/generate-sql', async (req, res) => {
+  try {
+    // eslint-disable-next-line no-use-before-define
+    const response = await getSql({ query: req.body });
+    res.json(response);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
