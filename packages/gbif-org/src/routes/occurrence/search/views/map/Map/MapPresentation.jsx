1dc32d41afa2979090d5348611bc5cf6c9b33930
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-11-06 14:23:32 +0100

    wip

--- packages/gbif-org/src/routes/occurrence/search/views/map/Map/MapPresentation.jsx
+++ packages/gbif-org/src/routes/occurrence/search/views/map/Map/MapPresentation.jsx
@@ -78,7 +78,7 @@ function Map({
 }) {
   // const dialog = useDialogState({ animated: true, modal: false });
   const config = useConfig();
-  const userLocationEnabled = true;//config?.occurrence?.mapSettings?.userLocationEnabled;
+  const userLocationEnabled = true; //config?.occurrence?.mapSettings?.userLocationEnabled;
 
   const styleLookup = config?.maps?.styleLookup || {};
 
@@ -117,7 +117,7 @@ function Map({
     refreshMode: 'debounce',
     refreshRate: 1000,
   });
-  
+
   useEffect(() => {
     console.log('generate basemap options');
     const mapStyles = getMapStyles({
@@ -133,8 +133,8 @@ function Map({
         language: config?.maps?.locale || 'en',
         mapComponents: {
           OpenlayersMap: MapComponentOL,
-          MapboxMap: () => <h1>mapbox map</h1> //MapComponentMB,
-        }
+          MapboxMap: () => <h1>mapbox map</h1>, //MapComponentMB,
+        },
       });
     }
     const newBaseMapOptions = Object.assign({}, mapStyles, mapStyleOverwrites);
@@ -269,9 +269,7 @@ function Map({
         {...{ style }}
       >
         {/* <ViewHeader message="counts.nResultsWithCoordinates" loading={loading} total={total} /> */}
-        <DynamicHeightDiv
-          minPxHeight={500}
-        >
+        <DynamicHeightDiv minPxHeight={500}>
           {/* {listVisible && <ListBox onCloseRequest={e => showList(false)}
           labelMap={labelMap}
           onClick={({ index }) => { dialog.show(); setActive(index) }}
@@ -324,8 +322,12 @@ function Map({
                 <DropdownMenuContent align="end">{menuLayerOptions}</DropdownMenuContent>
               </DropdownMenu>
             )}
-            
-          {userLocationEnabled && <MenuButton loading={searchingLocation} onClick={getUserLocation}><MdMyLocation /></MenuButton>}
+
+            {userLocationEnabled && (
+              <MenuButton loading={searchingLocation} onClick={getUserLocation}>
+                <MdMyLocation />
+              </MenuButton>
+            )}
           </div>
           <MapComponent
             {...mapProps}
@@ -355,15 +357,40 @@ function Map({
 
 export default Map;
 
-const MenuButton = React.forwardRef(({ children, ...props }, ref) => {
+const MenuButton = React.forwardRef(({ children, loading, ...props }, ref) => {
   return (
     <Button
       ref={ref}
       variant="ghost"
-      className="g-p-2 g-flex-auto g-text-2xl g-text-slate-800 g-whitespace-nowrap"
+      className="g-p-2 g-flex-auto g-text-xl g-text-slate-800 g-whitespace-nowrap"
       {...props}
     >
-      {children}
+      {loading ? <Spinner /> : children}
     </Button>
   );
 });
+
+function Spinner() {
+  return (
+    <div role="status">
+      <svg
+        aria-hidden="true"
+        className="g-text-gray-200 g-animate-spin dark:g-text-slate-300 g-fill-slate-500"
+        style={{height: '.8em', width: '.8em'}}
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span class="g-sr-only">Loading...</span>
+    </div>
+  );
+}
