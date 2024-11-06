d6e963c7343bb2840b15dc843f8172bffe86b194
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-11-06 09:45:23 +0100

    documentation

--- packages/gbif-org/src/components/DynamicHeightDiv.tsx
+++ packages/gbif-org/src/components/DynamicHeightDiv.tsx
@@ -5,7 +5,7 @@ export default function DynamicHeightDiv({
   minPxHeight = 300,
   maxPxHeight,
   sizeByViewport = false,
-  stepSize = 1,
+  stepSize = 20,
   children,
   style = {},
   ...props
@@ -24,31 +24,40 @@ export default function DynamicHeightDiv({
     const adjustHeight = () => {
       if (divRef.current) {
         // Get the position of the div relative to the viewport
-        const { top } = divRef.current.getBoundingClientRect();
+        const { top: relativetop } = divRef.current.getBoundingClientRect();
+        // get position relative to document top
+        const absoluteTop = window.scrollY + relativetop;
 
         // Calculate the remaining height
-        const availableHeight = window.innerHeight - top - (sizeByViewport ? 0 : window.scrollY);
+        // default behaviour is relative to the window, but it can be relative to the viewport. The latter will mean that the div will grow when scrolling
+        const availableHeight = window.innerHeight - (sizeByViewport ? relativetop : absoluteTop);
 
+        // get maximium height - the default is viewport height, but it can be set to a fixed value
         const maximumHeight = Math.max(minPxHeight, maxPxHeight ?? getDynamicViewportHeight());
-        // Set the height, ensuring it's at least 300px
+
+        // Set the height, ensuring it is between min and max heights
         const height = Math.max(minPxHeight, Math.min(availableHeight, maximumHeight));
-        setDivHeight(stepSize * Math.floor(height / stepSize)); // Round to nearest 10
+        // Allow rounding down to nearest step size (e.g. 10 px). This will prevent as much flickering when resizing
+        const stepHeight = stepSize * Math.floor(height / stepSize);
+        setDivHeight(stepHeight);
       }
     };
 
     // Initial height adjustment and on resize
     adjustHeight();
 
+    // add a debounce to avoid excessive recalculations
     const handleResize = debounce(adjustHeight, 30);
 
+    // watch for changes in the body size
     const resizeObserver = new ResizeObserver(() => {
       handleResize();
     });
+    resizeObserver.observe(document.body);
 
-    // Update the visible tab count on resize
     window.addEventListener('resize', handleResize);
     window.addEventListener('scroll', handleResize);
-    resizeObserver.observe(document.body);
+    
     return () => {
       window.removeEventListener('resize', handleResize);
       window.removeEventListener('scroll', handleResize);
