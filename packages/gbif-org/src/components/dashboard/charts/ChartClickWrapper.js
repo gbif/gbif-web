c1873a07b989fab3f23690419a615364b8faa730
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-11-20 16:23:12 +0100

    fix: charts filtering

--- packages/gbif-org/src/components/dashboard/charts/ChartClickWrapper.js
+++ packages/gbif-org/src/components/dashboard/charts/ChartClickWrapper.js
@@ -1,36 +1,29 @@
-import React, { useCallback } from 'react';
-// import qs from 'query-string';
-// import { useLocation, useHistory } from 'react-router-dom';
-// import { FilterContext } from '../../Filter/state';
-// import { mergeDeep } from '../../../utils/util';
+// @ts-nocheck
+import { FilterContext } from '@/contexts/filter';
+import React, { useCallback, useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { merge } from 'ts-deepmerge';
+import { stringify } from '@/utils/querystring';
 
-export default function ChartClickWrapper({ children, interactive, ...props }) {
-  // const { detailsRoute } = props;
+export default function ChartClickWrapper({ children, interactive, detailsRoute, ...props }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { filter: filterContext, setFilter } = useContext(FilterContext);
 
   const handleRedirect = useCallback(({ filter }) => {
-    return;
-  }, []);
+    if (!filter || !interactive) return;
+    if (!detailsRoute && !setFilter) {
+      console.warn('ChartClickWrapper: no detailsRoute or setFilter provided');
+      return;
+    }
+    const mergedFilter = merge({}, filterContext, { must: { ...filter } });
+    if (detailsRoute) {
+      const newLocation = `${detailsRoute || location.pathname}?${stringify(filter)}`;
+      navigate(newLocation, {replace: false});
+    } else {
+      setFilter(mergedFilter);
+    }
+  }, [detailsRoute, location.pathname, filterContext, interactive, setFilter, navigate]);
+
   return React.cloneElement(children, { handleRedirect, interactive, ...props });
-
-  // const location = useLocation();
-  // const history = useHistory();
-  // const { filter: filterContext, setFilter } = useContext(FilterContext);
-
-  // const handleRedirect = useCallback(({ filter }) => {
-  //   if (!filter || !interactive) return;
-  //   if (!detailsRoute && !setFilter) {
-  //     console.warn('ChartClickWrapper: no detailsRoute or setFilter provided');
-  //     return;
-  //   }
-
-  //   const mergedFilter = mergeDeep({}, filterContext, {must: {...filter}});
-  //   if (detailsRoute) {
-  //     const newLocation = `${detailsRoute || location.pathname}?${qs.stringify(filter)}`;
-  //     history.push(newLocation);
-  //   } else {
-  //     setFilter(mergedFilter);
-  //   }
-  // }, [detailsRoute, location.pathname, filterContext, interactive]);
-
-  // return React.cloneElement(children, { handleRedirect, interactive, ...props })
 }
