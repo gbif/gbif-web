70ac989d20604e2bddeb136670574712f111aa97
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-11-25 10:27:51 +0100

    avoid reloads on all path changes. relates to #657

--- packages/gbif-org/src/reactRouterPlugins/i18n/plugin.tsx
+++ packages/gbif-org/src/reactRouterPlugins/i18n/plugin.tsx
@@ -17,18 +17,23 @@ export function applyI18nPlugin(
   const defaultLanguage = config.languages.find((language) => language.default);
   if (!defaultLanguage) throw new Error('No default language found');
 
+  const translationsPromise = fetch(config.translationsEntryEndpoint)
+    .then((r) => r.json())
+    .catch((err) => {
+      console.error('Failed to load translations entry file');
+      throw err;
+    });
+    
   return config.languages.map((localeOption) => {
     return {
       description: `Root route for ${localeOption.label}`,
       path: defaultLanguage.code === localeOption.code ? '/' : localeOption.code,
+      shouldRevalidate() {
+        return false;
+      },
       loader: async () => {
         // fetch the entry translation file
-        const translations = await fetch(config.translationsEntryEndpoint)
-          .then((r) => r.json())
-          .catch((err) => {
-            console.error('Failed to load translations entry file');
-            throw err;
-          });
+        const translations = await translationsPromise;
         // now get the actual messages for the locale
         const messages = await fetch(
           translations?.[localeOption.code]?.messages ?? translations?.en?.messages
