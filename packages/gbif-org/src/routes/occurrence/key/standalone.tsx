ba4bce2ca7717e7dc2fdc855ef158b5bfc1eeeb7
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2024-12-11 13:42:14 +0100

    Fixed occurrence drawer history bug

--- packages/gbif-org/src/routes/occurrence/key/standalone.tsx
+++ packages/gbif-org/src/routes/occurrence/key/standalone.tsx
@@ -12,6 +12,7 @@ type Props = {
 export function StandaloneOccurrenceKeyPage({ occurrenceKey }: Props) {
   const [, setEntity] = useStringParam({ key: 'entity' });
   const setEntityParam = useRef((id: string) => {
+    console.log('setEntityParam', id);
     setEntity(`o_${id}`);
   });
 
@@ -28,10 +29,6 @@ export function StandaloneOccurrenceKeyPage({ occurrenceKey }: Props) {
   return <StandaloneWrapper routes={routes} url={`/occurrence/${occurrenceKey ?? 'loading'}`} />;
 }
 
-const extractOccurrenceKey = (pathname: string): string => {
-  return pathname.split('/occurrence/')[1].split('/')[0];
-};
-
 // This component is used to handle the case where the user navigates to another occurrence within the drawer.
 // The new occurrence will be displayed in the drawer, but the URL will not be updated.
 // Therefore, we need to update the URL when the user navigates to another occurrence.
@@ -43,15 +40,19 @@ function SyncDrawerOccurenceIdWithRootRouter({
   const drawerLocation = useLocation();
 
   useEffect(() => {
-    const rootOccurrenceKey = extractOccurrenceKey(window.location.pathname);
+    if (drawerLocation.pathname === '/occurrence/loading') return;
 
-    if (
-      drawerLocation.pathname !== '/occurrence/loading' ||
-      !drawerLocation.pathname.startsWith(`/occurrence/${rootOccurrenceKey}`)
-    ) {
-      const drawerOccurrenceKey = extractOccurrenceKey(drawerLocation.pathname);
-      setEntityParam.current(drawerOccurrenceKey);
-    }
+    const rootOccurrenceKey = new URLSearchParams(window.location.search)
+      .get('entity')
+      ?.split('o_')[1];
+    const drawerOccurrenceKey = drawerLocation.pathname.split('/occurrence/')[1].split('/')[0];
+
+    console.log('rootOccurrenceKey', rootOccurrenceKey);
+    console.log('drawerOccurrenceKey', drawerOccurrenceKey);
+
+    if (rootOccurrenceKey === drawerOccurrenceKey) return;
+
+    setEntityParam.current(drawerOccurrenceKey);
   }, [drawerLocation]);
 
   return <Outlet />;
