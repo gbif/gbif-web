3de00d4a3ea5489bc311f862a2cf4a6c2bb9b5a2
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-01-03 13:47:37 +0100

    watch defaultKey in url if any provided in filter config.  close #786

--- packages/gbif-org/src/dataManagement/filterAdapter/useFilterParams.ts
+++ packages/gbif-org/src/dataManagement/filterAdapter/useFilterParams.ts
@@ -37,7 +37,7 @@ export function useFilterParams({
   useEffect(() => {
     const fields = filterConfig?.fields ?? {};
     if (!isPlainObject(fields)) return;
-    setObservedParams([...Object.keys(fields), 'filter']);
+    setObservedParams([...Object.keys(fields).map((x) => fields?.[x]?.defaultKey ?? x), 'filter']);
 
     const empty: { [key: string]: undefined } = [...Object.keys(fields), ...remove].reduce(
       (accumulator: { [key: string]: undefined }, curr: string) => {
