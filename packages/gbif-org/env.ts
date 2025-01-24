861a73ae4caef8d55149e123e35c3871e0c7133c
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-01-24 14:11:32 +0100

    feat: warn developer if there are missing config keys

--- packages/gbif-org/env.ts
+++ packages/gbif-org/env.ts
@@ -1,11 +1,29 @@
 import { merge } from 'ts-deepmerge';
 import { loadEnv } from 'vite';
-import { getDefaultEndpointsBasedOnGbifEnv, GbifEnv } from './src/config/endpoints';
 
 const envFile = loadEnv('', process.cwd(), ['PUBLIC_']);
-export const graphqlEndpoint =
-  envFile.PUBLIC_GRAPHQL_ENDPOINT ??
-  getDefaultEndpointsBasedOnGbifEnv((envFile.PUBLIC_GBIF_ENV ?? 'prod') as GbifEnv).graphqlEndpoint;
+export const graphqlEndpoint = envFile.PUBLIC_GRAPHQL_ENDPOINT;
+
+const requiredFields = [
+  'PUBLIC_BASE_URL',
+  'PUBLIC_TRANSLATIONS_ENTRY_ENDPOINT',
+  'PUBLIC_GRAPHQL_ENDPOINT',
+  'PUBLIC_FORMS_ENDPOINT',
+  'PUBLIC_CONTENT_SEARCH',
+  'PUBLIC_WEB_UTILS',
+  'PUBLIC_API_V1',
+  'PUBLIC_API_V2',
+  'PUBLIC_TILE_API',
+  'PUBLIC_GBIF_ORG',
+  'PUBLIC_CHECKLIST_BANK_WEBSITE',
+  'PUBLIC_API_KEY_MAPTILER',
+  'PUBLIC_ENABLED_LANGUAGES',
+];
+requiredFields.forEach((field) => {
+  if (!envFile[field]) {
+    throw new Error(`Missing required field in env: ${field}`);
+  }
+});
 
 export function getEnv() {
   if (typeof window === 'undefined') {
