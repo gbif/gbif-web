82df47ae61d5022249ddde505e93b1884146213c
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-01-24 09:53:19 +0100

    more explicit tab style to avoid hp conflicts. see #864

--- packages/gbif-org/src/components/tabLink.tsx
+++ packages/gbif-org/src/components/tabLink.tsx
@@ -19,7 +19,7 @@ export function TabLink({ to, children, className, isActive, autoDetectActive }:
     cn(
       'g-flex g-leading-6 g-font-normal g-pt-3 g-pb-2.5 g-border-b-2 g-px-3',
       isActive
-        ? 'g-text-primary-500 g-border-current'
+        ? 'g-text-primary-500 g-border-current hover:g-text-primary-500 hover:g-border-current'
         : 'g-text-slate-900 g-border-transparent hover:g-border-slate-300 dark:g-text-slate-200 dark:hover:g-border-slate-700',
       className
     );
