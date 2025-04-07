e5b9168336e8f6a120354d5294be22f867959ee4
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-04-07 14:26:29 +0200

    Make shared pixelRatio compatible with our map apis

--- packages/gbif-org/src/utils/pixelRatio.ts
+++ packages/gbif-org/src/utils/pixelRatio.ts
@@ -1,4 +1,4 @@
 const max = 4;
-const fallback = 1;
+const dpr = window?.devicePixelRatio || 1;
 
-export const pixelRatio = Math.min(max, window?.devicePixelRatio || fallback);
+export const pixelRatio = Math.min(Math.ceil(dpr), max);
