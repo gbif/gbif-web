fb0b8ebe11defdfcf2580f24f3ef9ccb5727a650
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-04-11 10:20:21 +0200

    styling: data table background

--- packages/gbif-org/src/components/searchTable/table.tsx
+++ packages/gbif-org/src/components/searchTable/table.tsx
@@ -1,17 +1,17 @@
 import { useIsElementHorizontallyScrolled } from '@/hooks/useIsElementHorizontallyScrolled';
+import { Filters } from '@/routes/occurrence/search/filters';
 import { cn } from '@/utils/shadcn';
 import { memo, useEffect, useMemo, useRef } from 'react';
+import { ColumnDef } from '.';
+import { Table } from '../ui/table';
+import Body from './components/body/body';
 import { Footer } from './components/footer/footer';
-import { PaginationState } from './hooks/usePaginationState';
+import Header from './components/header/header';
 import { useColumnVisibility } from './hooks/useColumnVisibility';
 import { useFirstColumLock } from './hooks/useFirstColumLock';
 import { useOrderedColumns } from './hooks/useOrderedColumns';
-import Header from './components/header/header';
-import Body from './components/body/body';
-import { Table } from '../ui/table';
+import { PaginationState } from './hooks/usePaginationState';
 import { CreateRowLink } from './hooks/useRowLink';
-import { Filters } from '@/routes/occurrence/search/filters';
-import { ColumnDef } from '.';
 
 interface Props<T> {
   columns: ColumnDef<T>[];
@@ -82,7 +82,10 @@ function SearchTable<T>({
     <div
       className={cn('g-bg-white g-flex-1 g-border g-basis-full g-h-1 g-flex g-flex-col', className)}
     >
-      <div ref={tableWrapperRef} className="g-relative g-w-full g-overflow-auto g-h-full">
+      <div
+        ref={tableWrapperRef}
+        className="g-relative g-w-full g-overflow-auto g-h-full g-bg-slate-50"
+      >
         {/* https://limebrains.com/blog/2021-03-02T13:00-heigh-100-inside-table-td/ */}
         {/* Without this 1px height the a tags in the table cells won't be able to match the height of the td with height: 100% */}
         <Table className="g-h-1">
