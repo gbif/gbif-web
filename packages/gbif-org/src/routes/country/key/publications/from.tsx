a2c84c7899b9a40a18347ba418256c476a9b125d
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-04-30 12:52:04 +0200

    basic Publications from tab

--- packages/gbif-org/src/routes/country/key/publications/from.tsx
+++ packages/gbif-org/src/routes/country/key/publications/from.tsx
@@ -1,9 +1,54 @@
+import { useConfig } from '@/config/config';
+import { FilterProvider } from '@/contexts/filter';
+import { SearchContextProvider, SearchMetadata } from '@/contexts/search';
+import { useFilterParams } from '@/dataManagement/filterAdapter/useFilterParams';
+import { LiteratureSearchInner } from '@/routes/literature/search/literatureSearch';
+import { searchConfig } from '@/routes/literature/search/searchConfig';
+import { ArticleContainer } from '@/routes/resource/key/components/articleContainer';
+import { ArticleTextContainer } from '@/routes/resource/key/components/articleTextContainer';
+import { useEffect, useState } from 'react';
+
 export function CountryKeyPublicationsFrom() {
+  const [filter, setFilter] = useFilterParams({
+    filterConfig: searchConfig,
+    paramsToRemove: ['offset'],
+  });
+  const baseConfig = useConfig();
+  const [config, setConfig] = useState<SearchMetadata | undefined>();
+  const literatureSearchConfig = baseConfig?.datasetKey?.literatureSearch;
+
+  useEffect(() => {
+    const c = {
+      ...baseConfig.literatureSearch,
+      ...literatureSearchConfig,
+      excludedFilters: [
+        ...(literatureSearchConfig?.excludedFilters ?? []),
+        'countriesOfResearcher',
+      ],
+      highlightedFilters: [
+        ...(literatureSearchConfig?.highlightedFilters ?? []),
+        'countriesOfResearcher',
+      ],
+      scope: {
+        type: 'equals',
+        key: 'countriesOfResearcher',
+        value: 'DK',
+      },
+    };
+    setConfig(c);
+  }, [baseConfig, literatureSearchConfig]);
+
   return (
-    <div>
-      <h1>Country Key Publications From</h1>
-      <p>This is the publications from page for the country key.</p>
-      {/* Add more content here as needed */}
-    </div>
+    <ArticleContainer className="g-bg-slate-100">
+      <ArticleTextContainer className="g-max-w-screen-xl">
+        {config && (
+          <SearchContextProvider searchContext={config}>
+            <FilterProvider filter={filter} onChange={setFilter}>
+              <LiteratureSearchInner />
+            </FilterProvider>
+          </SearchContextProvider>
+        )}
+      </ArticleTextContainer>
+    </ArticleContainer>
   );
 }
