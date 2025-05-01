9c8a2fdcbb0f02c92b4098cfe70e072bf488120b
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-05-01 11:39:51 +0200

    basic country data publishing tab

--- packages/gbif-org/src/routes/country/key/publishing.tsx
+++ packages/gbif-org/src/routes/country/key/publishing.tsx
@@ -1,9 +1,18 @@
+import { ArticleContainer } from '@/routes/resource/key/components/articleContainer';
+import { ArticleTextContainer } from '@/routes/resource/key/components/articleTextContainer';
+import { useParams } from 'react-router-dom';
+import { DataFromCountryMap } from './components/dataFromCountryMap';
+
 export function CountryKeyPublishing() {
+  const { countryCode } = useParams();
+
+  if (!countryCode) throw new Error('Country code is required');
+
   return (
-    <div>
-      <h1>Country Key Publishing</h1>
-      <p>This is the publishing page for the country key.</p>
-      {/* Add more content here as needed */}
-    </div>
+    <ArticleContainer className="g-bg-slate-100 g-pt-4">
+      <ArticleTextContainer className="g-max-w-screen-xl g-flex g-flex-col g-gap-4">
+        <DataFromCountryMap countryCode={countryCode} />
+      </ArticleTextContainer>
+    </ArticleContainer>
   );
 }
