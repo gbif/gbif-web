d459bbe6bdff1800d8780c87d9b33d40cf8f3053
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-05-28 14:55:34 +0200

    fix: cache webgl support

--- packages/gbif-org/src/utils/isWebglSupported.ts
+++ packages/gbif-org/src/utils/isWebglSupported.ts
@@ -1,4 +1,16 @@
+let isWebglSupportedCache: boolean | null = null;
+
 export function isWebglSupported() {
+  if (isWebglSupportedCache !== null) {
+    return isWebglSupportedCache;
+  }
+  isWebglSupportedCache = checkWebglSupport();
+  return isWebglSupportedCache;
+}
+
+// check webgl support and dispose canvas afterwards
+function checkWebglSupport() {
+  // Check if the browser supports WebGL
   if (window.WebGLRenderingContext) {
     const canvas = document.createElement('canvas');
     try {
@@ -8,11 +20,14 @@ export function isWebglSupported() {
       // for more details.
       const context = canvas.getContext('webgl2') || canvas.getContext('webgl');
       if (context && typeof context.getParameter == 'function') {
+        // WebGL is supported
+        canvas.remove(); // Clean up the canvas element
         return true;
       }
     } catch (e) {
       // WebGL is supported, but disabled
     }
+    canvas.remove(); // Clean up the canvas element
     return false;
   }
   // WebGL not supported
