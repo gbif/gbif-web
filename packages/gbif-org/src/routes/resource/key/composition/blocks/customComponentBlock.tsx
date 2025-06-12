657ce06beb0f1172e0f376f31210bec5167ec23b
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-06-12 14:22:09 +0200

    feat: close https://github.com/gbif/gbif-web/issues/728

--- packages/gbif-org/src/routes/resource/key/composition/blocks/customComponentBlock.tsx
+++ packages/gbif-org/src/routes/resource/key/composition/blocks/customComponentBlock.tsx
@@ -3,6 +3,7 @@ import { fragmentManager } from '@/services/fragmentManager';
 import { backgroundColorMap, BlockContainer } from './_shared';
 import { HostedPortalForm } from './customComponents/hostedPortalForm';
 import { ProjectsTable } from './customComponents/projects';
+import { PublisherDatasetTable } from './customComponents/publisherDatasetTable';
 
 fragmentManager.register(/* GraphQL */ `
   fragment CustomComponentBlockDetails on CustomComponentBlock {
@@ -15,17 +16,26 @@ fragmentManager.register(/* GraphQL */ `
   }
 `);
 
+export const widthMap: Record<string, string> = {
+  narrow: 'g-max-w-3xl g-m-auto',
+  normal: 'g-max-w-5xl g-m-auto',
+  wide: 'g-max-w-6xl g-m-auto',
+  fluid: 'g-max-w-full g-m-auto',
+};
+
 type Props = {
   resource: CustomComponentBlockDetailsFragment;
 };
 
 export function CustomComponentBlock({ resource }: Props) {
   const backgroundColor = backgroundColorMap[resource?.backgroundColour ?? 'white'];
-
+  const width = widthMap[resource?.width ?? 'normal'];
   return (
     <BlockContainer className={`${backgroundColor} g-border-t g-border-slate-100 g-p-0`}>
       {/* <ArticleTextContainer> */}
-      <CustomComponent resource={resource} />
+      <div className={width}>
+        <CustomComponent resource={resource} />
+      </div>
       {/* </ArticleTextContainer> */}
     </BlockContainer>
   );
@@ -50,6 +60,14 @@ function CustomComponent({
           className="g-py-8"
           programmeId={resource?.settings?.programmeId}
           tableStyle={resource?.settings?.tablestyle}
+        />
+      );
+    case 'publisherOrDatasetTable':
+      return (
+        <PublisherDatasetTable
+          className="g-py-8"
+          settings={resource?.settings}
+          title={resource?.title}
         />
       );
     default:
