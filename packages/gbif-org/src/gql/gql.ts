e2d4d677682166e7856512a7eef96becbb5b5985
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-06-20 12:54:22 +0200

    update schema

--- packages/gbif-org/src/gql/gql.ts
+++ packages/gbif-org/src/gql/gql.ts
@@ -51,6 +51,7 @@ const documents = {
     "\n    query DatasetPublishingCountryFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: publishingCountry {\n            name\n            count\n          }\n        }\n      }\n    }\n  ": types.DatasetPublishingCountryFacetDocument,
     "\n    query DatasetLicenceFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: license {\n            name\n            count\n          }\n        }\n      }\n    }\n  ": types.DatasetLicenceFacetDocument,
     "\n    query DatasetTypeFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: type {\n            name\n            count\n          }\n        }\n      }\n    }\n  ": types.DatasetTypeFacetDocument,
+    "\n    query DataseNetworkFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: networkKey {\n            name\n            count\n          }\n        }\n      }\n    }\n  ": types.DataseNetworkFacetDocument,
     "\n    query DatasetDwcaExtensionFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: dwcaExtension {\n            name\n            count\n          }\n        }\n      }\n    }\n  ": types.DatasetDwcaExtensionFacetDocument,
     "\n  fragment HomePageCountIcons on Home {\n    occurrenceIcon {\n      file {\n        url\n      }\n    }\n    datasetIcon {\n      file {\n        url\n      }\n    }\n    publisherIcon {\n      file {\n        url\n      }\n    }\n    literatureIcon {\n      file {\n        url\n      }\n    }\n  }\n": types.HomePageCountIconsFragmentDoc,
     "\n  query HomePage {\n    gbifHome {\n      title\n      summary\n      primaryImage {\n        file {\n          url\n          thumbor\n        }\n        title\n        description\n      }\n      ...HomePageCountIcons\n      blocks {\n        ...BlockItemDetails\n      }\n    }\n  }\n": types.HomePageDocument,
@@ -372,6 +373,10 @@ export function graphql(source: "\n    query DatasetLicenceFacet($query: Dataset
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n    query DatasetTypeFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: type {\n            name\n            count\n          }\n        }\n      }\n    }\n  "): (typeof documents)["\n    query DatasetTypeFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: type {\n            name\n            count\n          }\n        }\n      }\n    }\n  "];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n    query DataseNetworkFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: networkKey {\n            name\n            count\n          }\n        }\n      }\n    }\n  "): (typeof documents)["\n    query DataseNetworkFacet($query: DatasetSearchInput) {\n      search: datasetSearch(query: $query) {\n        facet {\n          field: networkKey {\n            name\n            count\n          }\n        }\n      }\n    }\n  "];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
