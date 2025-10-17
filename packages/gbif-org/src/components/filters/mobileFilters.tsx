38eba69251bc37b9021442f5a9cbea21a37478e0
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-10-17 14:13:43 +0200

    Don't display filter dialog icon if there are no filters in the dialog

--- packages/gbif-org/src/components/filters/mobileFilters.tsx
+++ packages/gbif-org/src/components/filters/mobileFilters.tsx
@@ -48,28 +48,30 @@ export function MobileFilters({ filters, groups, className }: MobileFiltersProps
   return (
     <div className={cn('g-flex g-flex-1 g-flex-row g-justify-between', className)}>
       {highlightedFreeTextFilter?.Button && <highlightedFreeTextFilter.Button />}
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button
-            size="sm"
-            variant="ghost"
-            className="g-relative g-px-1 g-mb-1 g-text-slate-400 g-ml-auto"
+      {Object.keys(otherFilters).length > 0 && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              size="sm"
+              variant="ghost"
+              className="g-relative g-px-1 g-mb-1 g-text-slate-400 g-ml-auto"
+            >
+              <FilterIcon className="g-text-base" />
+              {activeFilterCount > 0 && (
+                <span className="g-absolute -g-top-1 -g-right-1 g-bg-primary-500 g-text-white g-text-xs g-rounded-full g-size-5 g-flex g-items-center g-justify-center g-font-medium">
+                  <FormattedNumber value={activeFilterCount} />
+                </span>
+              )}
+            </Button>
+          </DialogTrigger>
+          <DialogBottomSheetContent
+            className="g-w-full g-p-0"
+            onOpenAutoFocus={(e) => e.preventDefault()}
           >
-            <FilterIcon className="g-text-base" />
-            {activeFilterCount > 0 && (
-              <span className="g-absolute -g-top-1 -g-right-1 g-bg-primary-500 g-text-white g-text-xs g-rounded-full g-size-5 g-flex g-items-center g-justify-center g-font-medium">
-                <FormattedNumber value={activeFilterCount} />
-              </span>
-            )}
-          </Button>
-        </DialogTrigger>
-        <DialogBottomSheetContent
-          className="g-w-full g-p-0"
-          onOpenAutoFocus={(e) => e.preventDefault()}
-        >
-          <MobileFilterDrawerContent filters={otherFilters} groups={groups} />
-        </DialogBottomSheetContent>
-      </Dialog>
+            <MobileFilterDrawerContent filters={otherFilters} groups={groups} />
+          </DialogBottomSheetContent>
+        </Dialog>
+      )}
     </div>
   );
 }
