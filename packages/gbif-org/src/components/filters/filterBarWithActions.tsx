5748f813d2bd6299f8afcc9ff41010d3e7c6efc8
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-10-17 14:41:14 +0200

    Only use the mobile filter drawer when there are 3 or more filters

--- packages/gbif-org/src/components/filters/filterBarWithActions.tsx
+++ packages/gbif-org/src/components/filters/filterBarWithActions.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 import { FilterContext } from '@/contexts/filter';
 import { useSearchContext } from '@/contexts/search';
 import { cn } from '@/utils/shadcn';
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { MdDeleteOutline } from 'react-icons/md';
 import { FilterBar, FilterButtons } from './filterTools';
 import { MobileFilters } from './mobileFilters';
@@ -24,13 +24,25 @@ export function FilterBarWithActions({
   const filterContext = useContext(FilterContext);
   const searchContext = useSearchContext();
 
+  // Count visible filters, accounting for excluded filters
+  const visibleFilterCount = useMemo(() => {
+    return Object.keys(filters).filter(
+      (filter) => !searchContext?.excludedFilters?.includes(filter)
+    ).length;
+  }, [filters, searchContext]);
+
+  // If 2 or fewer filters, always show FilterButtons and never show MobileFilters
+  const shouldShowMobileFilters = visibleFilterCount > 2;
+
   return (
     <FilterBar className={cn('g-flex f-flex-nowrap g-items-start g-gap-2', className)}>
-      <div className="g-hidden sm:g-block">
+      <div className={cn(shouldShowMobileFilters && 'g-hidden sm:g-block')}>
         <FilterButtons filters={filters} searchContext={searchContext} groups={groups} />
       </div>
       <div className="g-flex g-items-center g-gap-1 g-flex-1 g-justify-end">
-        <MobileFilters className="sm:g-hidden" filters={filters} groups={groups} />
+        {shouldShowMobileFilters && (
+          <MobileFilters className="sm:g-hidden" filters={filters} groups={groups} />
+        )}
         {additionalActions}
         <Button
           size="sm"
