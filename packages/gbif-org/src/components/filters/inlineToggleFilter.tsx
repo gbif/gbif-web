d49c60713dccc8556f71ca7e9463dc84e4220e79
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-10-29 14:28:35 +0100

    inline toggle filter styling

--- packages/gbif-org/src/components/filters/inlineToggleFilter.tsx
+++ packages/gbif-org/src/components/filters/inlineToggleFilter.tsx
@@ -27,28 +27,16 @@ export function InlineToggleFilter({
       className={cn('g-mx-1 g-mb-1 g-h-8 g-overflow-hidden', className)}
       variant="primary"
     >
-      {options.map((option, index) => {
-        const isLast = index === options.length - 1;
-        const isFirst = index === 0;
-
-        return (
-          <ToggleGroupItem
-            key={option.value}
-            value={option.value}
-            variant="primary"
-            className={cn(
-              'g-h-8 g-py-0 g-flex g-items-center g-justify-center g-px-3 !g-border-0',
-              {
-                'g-border-r g-border-r-solid g-border-r-primary-500 data-[state=on]:g-border-r-primary-600':
-                  !isLast,
-              },
-              { 'g-rounded-none': isFirst || isLast }
-            )}
-          >
-            <FormattedMessage id={option.labelKey} defaultMessage={option.value} />
-          </ToggleGroupItem>
-        );
-      })}
+      {options.map((option) => (
+        <ToggleGroupItem
+          key={option.value}
+          value={option.value}
+          variant="primary"
+          className="g-h-8 g-py-0 g-px-3 g-flex g-items-center g-justify-center g-border-0 data-[state=off]:g-text-slate-600 g-ring-inset g-rounded-none"
+        >
+          <FormattedMessage id={option.labelKey} defaultMessage={option.value} />
+        </ToggleGroupItem>
+      ))}
     </ToggleGroup>
   );
 }
