05a542fffb638da836106ee8f034ee23e1b6dec5
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-11-25 18:35:05 +0100

    #724 service agreement and data processor agreement as links

--- packages/gbif-org/src/routes/resource/key/composition/blocks/customComponents/hostedPortalForm/steps/terms.tsx
+++ packages/gbif-org/src/routes/resource/key/composition/blocks/customComponents/hostedPortalForm/steps/terms.tsx
@@ -9,7 +9,29 @@ export function Terms() {
       label={
         <FormattedMessage
           id="hostedPortalApplication.termsLabel"
-          defaultMessage="I have read the service agreement and data processor agreement and I accept these terms and conditions for the hosted portal I plan to launch."
+          defaultMessage="I have read the <service-agreement-link>service agreement</service-agreement-link> and <data-processor-agreement-link>data processor agreement</data-processor-agreement-link> and I accept these terms and conditions for the hosted portal I plan to launch."
+          values={{
+            'service-agreement-link': (text) => (
+              <a
+                href="/terms/hosted-portal/service-agreement"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="g-text-primary-500 hover:g-text-primary-700 g-underline"
+              >
+                {text}
+              </a>
+            ),
+            'data-processor-agreement-link': (text) => (
+              <a
+                href="/terms/hosted-portal/data-processor"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="g-text-primary-500 hover:g-text-primary-700 g-underline"
+              >
+                {text}
+              </a>
+            ),
+          }}
         />
       }
     />
