33cb9d174942e6399a5ad95b31b317ecdc0b7d02
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-11-25 20:05:13 +0100

    Disable fuzzy search on directory people (the results are not sorted, so it just allows for bad results to show up when using fuzzy search)

--- packages/gbif-org/src/routes/custom/contact-us/tabels/utils/searching.tsx
+++ packages/gbif-org/src/routes/custom/contact-us/tabels/utils/searching.tsx
@@ -1,20 +1,38 @@
 import { matchSorter } from 'match-sorter';
 import { useMemo } from 'react';
 
-type Options<T> = { q: string; searchKeys: string[]; items: T[] | Record<string, T> };
+type Options<T> = {
+  q: string;
+  searchKeys: string[];
+  items: T[] | Record<string, T>;
+  fuzzySearch?: boolean;
+};
 
-export function useFilteredItems<T>({ q, searchKeys, items }: Options<T>): T[] {
+export function useFilteredItems<T>({
+  q,
+  searchKeys,
+  items,
+  fuzzySearch = false,
+}: Options<T>): T[] {
   return useMemo(() => {
     let results = Array.isArray(items) ? items : Object.values(items);
 
     if (q) {
-      results = matchSorter(results, q, {
-        keys: searchKeys,
-        // No need to sort based on best match when using table sorting
-        sorter: (x) => x,
-      });
+      if (fuzzySearch) {
+        results = matchSorter(results, q, {
+          keys: searchKeys,
+          // No need to sort based on best match when using table sorting
+          sorter: (x) => x,
+        });
+      } else {
+        results = results.filter((item) =>
+          searchKeys.some((key) =>
+            item[key as keyof T]?.toString().toLowerCase().includes(q.toLowerCase())
+          )
+        );
+      }
     }
 
     return results;
-  }, [items, q, searchKeys]);
+  }, [items, q, searchKeys, fuzzySearch]);
 }
