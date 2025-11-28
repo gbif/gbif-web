3ba7a9ad8be18f885665319862e558680b6332b8
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-11-28 16:08:30 +0100

    only require the CYPRESS_BASE_URL for running tests with cypress

--- packages/gbif-org/cypress.config.ts
+++ packages/gbif-org/cypress.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'cypress';
-import { getEnv } from './env';
 import viteConfigFn from './gbif/vite.config';
 
-const env = getEnv();
 const viteConfig = viteConfigFn({ command: 'serve' });
 
 export default defineConfig({
   e2e: {
     // Can be overriden by CYPRESS_BASE_URL env variable
-    baseUrl: env.PUBLIC_BASE_URL,
+    baseUrl: process.env.CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
