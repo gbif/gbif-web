805ac4df064f55501622d3a66b53fe5c2dec5df0
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2025-12-10 16:17:43 +0100

    close https://github.com/gbif/gbif-web/issues/1417

--- packages/gbif-org/src/components/select/countryCodeSelect.tsx
+++ packages/gbif-org/src/components/select/countryCodeSelect.tsx
@@ -1,31 +1,30 @@
-import countries from '@/enums/basic/country.json';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
+import { useSortedCountries } from '@/hooks/useSortedCountries';
+
 type Props = {
   selected?: string;
   onChange(value: string): void;
 };
 
 export function CountryCodeSelect({ selected, onChange }: Props) {
-  const intl = useIntl();
+  const { formatMessage } = useIntl();
+  const sortedCountries = useSortedCountries();
 
   return (
     <Select value={selected} onValueChange={onChange}>
       <SelectTrigger>
         <SelectValue
-          placeholder={intl.formatMessage({
+          placeholder={formatMessage({
             id: 'eoi.pleaseSelectCountry',
             defaultMessage: 'Select a country',
           })}
         />
       </SelectTrigger>
       <SelectContent>
-        {countries.map((countryCode) => (
-          <SelectItem key={countryCode} value={countryCode}>
-            <FormattedMessage
-              id={`enums.countryCode.${countryCode}`}
-              defaultMessage={countryCode}
-            />
+        {sortedCountries.map((country) => (
+          <SelectItem key={country.key} value={country.key}>
+            {country.title}
           </SelectItem>
         ))}
       </SelectContent>
