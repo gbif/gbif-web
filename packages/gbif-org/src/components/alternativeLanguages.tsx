dfc47e869473411158b345bd0d61c1da122fc9b0
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2026-03-02 15:40:59 +0100

    include current lang in alternative languages metadata

--- packages/gbif-org/src/components/alternativeLanguages.tsx
+++ packages/gbif-org/src/components/alternativeLanguages.tsx
@@ -5,21 +5,19 @@ import { useLocation } from 'react-router-dom';
 
 export function AlternativeLanguages() {
   const config = useConfig();
-  const { locale, defaultLocale, localizeLink } = useI18n();
+  const { defaultLocale, localizeLink } = useI18n();
   const location = useLocation();
 
   return (
     <Helmet>
-      {config.languages
-        .filter((l) => l.code !== locale.code)
-        .map((l) => (
-          <link
-            key={l.code}
-            rel="alternate"
-            hrefLang={l.code}
-            href={config.baseUrl + localizeLink(location.pathname, l.code) + location.search}
-          />
-        ))}
+      {config.languages.map((l) => (
+        <link
+          key={l.code}
+          rel="alternate"
+          hrefLang={l.code}
+          href={config.baseUrl + localizeLink(location.pathname, l.code) + location.search}
+        />
+      ))}
       <link
         rel="alternate"
         hrefLang="x-default"
