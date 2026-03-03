4f318e0faf75745fbd2d120c28d38cf3a1c98f8d
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2026-03-03 20:27:49 +0100

    bare bones custom /ipt page #719

---
+++ packages/gbif-org/src/routes/custom/ipt.tsx
@@ -0,0 +1,122 @@
+import { IptPageQuery } from '@/gql/graphql';
+import { LoaderArgs, RouteObjectWithPlugins } from '@/reactRouterPlugins';
+import { ArticleBanner } from '@/routes/resource/key/components/articleBanner';
+import { FormattedMessage } from 'react-intl';
+import { useLoaderData } from 'react-router-dom';
+import { ArticleAuxiliary } from '../resource/key/components/articleAuxiliary';
+import { ArticleBody } from '../resource/key/components/articleBody';
+import { ArticleFooterWrapper } from '../resource/key/components/articleFooterWrapper';
+import { ArticleIntro } from '../resource/key/components/articleIntro';
+import { ArticleSkeleton } from '../resource/key/components/articleSkeleton';
+import { ArticleTags } from '../resource/key/components/articleTags';
+import { ArticleTextContainer } from '../resource/key/components/articleTextContainer';
+import { ArticleTitle } from '../resource/key/components/articleTitle';
+import { Documents } from '../resource/key/components/documents';
+import { PageContainer } from '../resource/key/components/pageContainer';
+import { SecondaryLinks } from '../resource/key/components/secondaryLinks';
+import PageMetaData from '@/components/PageMetaData';
+
+const IPT_QUERY = /* GraphQL */ `
+  query IptPage {
+    resource(alias: "/ipt") {
+      __typename
+      ... on Article {
+        id
+        title
+        summary
+        excerpt
+        body
+        primaryImage {
+          ...ArticleBanner
+        }
+        secondaryLinks {
+          label
+          url
+        }
+        documents {
+          ...DocumentPreview
+        }
+        topics
+        purposes
+        audiences
+        citation
+        createdAt
+      }
+    }
+  }
+`;
+
+function iptPageLoader(args: LoaderArgs) {
+  return args.graphql.query<IptPageQuery, undefined>(IPT_QUERY, undefined);
+}
+
+function IptPage() {
+  const { data } = useLoaderData() as { data: IptPageQuery };
+  const resource = data.resource;
+
+  if (resource?.__typename !== 'Article') {
+    throw new Error('Invalid resource type');
+  }
+
+  return (
+    <article>
+      <PageMetaData
+        title={resource.title}
+        description={resource.excerpt ?? resource.summary}
+        path="/ipt"
+      />
+
+      <PageContainer topPadded bottomPadded className="g-bg-white">
+        <ArticleTextContainer className="g-mb-10">
+          <ArticleTitle dangerouslySetTitle={{ __html: resource.title }} />
+
+          {resource.summary && (
+            <ArticleIntro dangerouslySetIntro={{ __html: resource.summary }} className="g-mt-2" />
+          )}
+        </ArticleTextContainer>
+
+        <ArticleBanner className="g-mt-8 g-mb-6" image={resource?.primaryImage} />
+
+        <ArticleTextContainer>
+          {resource.body && (
+            <ArticleBody dangerouslySetBody={{ __html: resource.body }} className="g-mt-2" />
+          )}
+
+          <ArticleFooterWrapper>
+            {resource.secondaryLinks && (
+              <ArticleAuxiliary>
+                <SecondaryLinks links={resource.secondaryLinks} className="g-mt-8" />
+              </ArticleAuxiliary>
+            )}
+
+            {resource.documents && (
+              <ArticleAuxiliary>
+                <Documents documents={resource.documents} className="g-mt-8" />
+              </ArticleAuxiliary>
+            )}
+
+            {resource.citation && (
+              <ArticleAuxiliary
+                label={<FormattedMessage id="cms.auxiliary.citation" />}
+                dangerouslySetValue={{
+                  __html: resource.citation,
+                  classNames: 'underlineLinks',
+                }}
+              />
+            )}
+
+            <ArticleTags resource={resource} className="g-mt-8" />
+          </ArticleFooterWrapper>
+        </ArticleTextContainer>
+      </PageContainer>
+    </article>
+  );
+}
+
+export const iptRoute: RouteObjectWithPlugins = {
+  id: 'ipt',
+  element: <IptPage />,
+  loader: iptPageLoader,
+  loadingElement: <ArticleSkeleton />,
+  path: 'ipt',
+};
