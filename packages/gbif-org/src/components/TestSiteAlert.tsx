67f79cca956951b92f15e62c91284bb970a33d0f
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2026-05-19 15:40:43 +0200

    rtl styling

--- packages/gbif-org/src/components/TestSiteAlert.tsx
+++ packages/gbif-org/src/components/TestSiteAlert.tsx
@@ -1,13 +1,14 @@
 import { FormattedMessage } from 'react-intl';
 import { Alert } from './ui/alert';
-import { cn } from '@/utils/shadcn';
 
 export default function TestSiteAlert({ className }: { className?: string }) {
   const testSite = import.meta.env.PUBLIC_TEST_SITE === 'true';
   if (!testSite) return null;
   return (
-    <Alert variant="destructive" className={cn('g-text-site-dir-start', className)} dir="auto">
-      <FormattedMessage id="phrases.testSiteWarning" />
+    <Alert variant="destructive" className={className} dir="auto">
+      <div className="g-text-site-dir-start">
+        <FormattedMessage id="phrases.testSiteWarning" />
+      </div>
     </Alert>
   );
 }
