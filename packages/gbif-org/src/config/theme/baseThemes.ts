b42ca96021ffd7b11857b574839f8810588894d7
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2026-05-29 16:13:13 +0200

    feat: align hp and theme fonts. close #1491

--- packages/gbif-org/src/config/theme/baseThemes.ts
+++ packages/gbif-org/src/config/theme/baseThemes.ts
@@ -11,9 +11,9 @@ export const defaultTheme = {
   color: '#162d3d',
   isDarkTheme: false,
   fontFamily:
-    '"Helvetica Neue", BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica", "Arial", sans-serif',
+    'BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
   headerFontFamily:
-    '"Helvetica Neue", BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica", "Arial", sans-serif',
+    'BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
   // fontFamily: "'Inter var', BlinkMacSystemFont, -apple-system, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
   borderRadius: 3,
   drawerZIndex: 50,
