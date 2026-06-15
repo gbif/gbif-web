974ca8d1edb718ec1d63723607eb1e527d09d331
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2026-06-15 13:42:14 +0200

    add fallback /api/resource/search endpoint (#1805)
    The old website had a proxy API. In case something still calls it we remap it.
    At some point we should add logging and monitor if it is still being used. For now it is okay to just handle it, we have more pressing matters

--- packages/gbif-org/gbif/server.js
+++ packages/gbif-org/gbif/server.js
@@ -13,6 +13,7 @@ import { register as registerRobots } from './routes/robots/index.mjs';
 import { register as registerSitemaps } from './routes/sitemaps/endpoints.mjs';
 import { register as registerUser } from './routes/user/endpoints.mjs';
 import { register as registerProxies } from './routes/proxy/proxy.mjs';
+import { register as registerResourceSearch } from './routes/resourceSearch/endpoints.mjs';
 import createGetRedirect from './middleware/redirects.mjs';
 // Load environment variables from .env files and merge them with process.env.
 const envFile = loadEnv('', process.cwd(), ['PUBLIC_']);
@@ -112,6 +113,7 @@ async function main() {
 
   registerUser(app);
   registerProxies(app);
+  registerResourceSearch(app);
   registerSitemaps(app);
   registerRobots(app);
 
