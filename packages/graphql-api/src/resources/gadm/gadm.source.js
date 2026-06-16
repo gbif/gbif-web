903d458f5b46912eadcd39d339fd1f3cda6d83f6
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2026-06-16 21:26:16 +0200

    chore: graphql - forward relevant headers upstream

--- packages/graphql-api/src/resources/gadm/gadm.source.js
+++ packages/graphql-api/src/resources/gadm/gadm.source.js
@@ -10,8 +10,11 @@ class GadmAPI extends RESTDataSource {
 
   willSendRequest(path, request) {
     request.headers['User-Agent'] = this.context.userAgent;
-    request.headers['referer'] = this.context.referer;
-    request.headers['x-client-priority'] = this.context.clientPriority;
+    if (this.context.referer) request.headers['referer'] = this.context.referer;
+    if (this.context.clientPriority) request.headers['x-client-priority'] = this.context.clientPriority;
+    if (this.context.siteUrl) request.headers['x-gbif-site-url'] = this.context.siteUrl;
+    if (this.context.requestId) request.headers['x-request-id'] = this.context.requestId;
+    if (this.context.clientIp) request.headers['x-client-ip'] = this.context.clientIp;
     request.agent = getDefaultAgent(this.baseURL, path);
   }
 
--- packages/graphql-api/src/resources/taxon/taxon.source.js
+++ packages/graphql-api/src/resources/taxon/taxon.source.js
@@ -15,8 +15,11 @@ class TaxonAPI extends QueuedRESTDataSource {
 
   willSendRequest(path, request) {
     request.headers['User-Agent'] = this.context.userAgent;
-    request.headers['referer'] = this.context.referer;
-    request.headers['x-client-priority'] = this.context.clientPriority;
+    if (this.context.referer) request.headers['referer'] = this.context.referer;
+    if (this.context.clientPriority) request.headers['x-client-priority'] = this.context.clientPriority;
+    if (this.context.siteUrl) request.headers['x-gbif-site-url'] = this.context.siteUrl;
+    if (this.context.requestId) request.headers['x-request-id'] = this.context.requestId;
+    if (this.context.clientIp) request.headers['x-client-ip'] = this.context.clientIp;
     request.agent = getTaxonAgent(this.baseURL, path);
   }
 
