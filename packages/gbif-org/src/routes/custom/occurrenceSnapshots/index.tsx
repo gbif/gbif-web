9d23a0fa1f4e85273eab0dc31958951189c86d27
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2026-07-06 17:39:37 +0200

    styling: occurrence-snapshots align with prose pages #1822

--- packages/gbif-org/src/routes/custom/occurrenceSnapshots/index.tsx
+++ packages/gbif-org/src/routes/custom/occurrenceSnapshots/index.tsx
@@ -4,6 +4,7 @@ import { OccurrenceSnapshotsQuery, OccurrenceSnapshotsQueryVariables } from '@/g
 import useQuery from '@/hooks/useQuery';
 import { useEffect, useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
+import { ArticleIntro } from '../../resource/key/components/articleIntro';
 import { ArticleTextContainer } from '../../resource/key/components/articleTextContainer';
 import { ArticleTitle } from '../../resource/key/components/articleTitle';
 import { PageContainer } from '../../resource/key/components/pageContainer';
@@ -57,57 +58,59 @@ const OccurrenceSnapshots = () => {
       />
 
       <PageContainer topPadded className="g-bg-white g-pb-10">
-        <ArticleTextContainer className="g-mb-2">
+        <ArticleTextContainer className="g-mb-10">
           <ArticleTitle>
             <FormattedMessage id="occurrenceSnapshots.title" />
           </ArticleTitle>
+          <ArticleIntro className="g-mt-2">
+            <FormattedMessage id="occurrenceSnapshots.intro" />
+          </ArticleIntro>
         </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-10">
-          <FormattedMessage id="occurrenceSnapshots.intro" />
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-10">
-          <FormattedMessage id="occurrenceSnapshots.description" />
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-2">
-          <ArticleTitle>
-            <FormattedMessage id="occurrenceSnapshots.cloudDatasets.title" />
-          </ArticleTitle>
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-8">
-          <FormattedMessage
-            id="occurrenceSnapshots.cloudDatasets.description"
-            values={{
-              azure: link('https://planetarycomputer.microsoft.com/dataset/gbif'),
-              aws: link('https://registry.opendata.aws/gbif/'),
-              gcs: link('https://console.cloud.google.com/storage/browser/public-datasets-gbif'),
-              bq: link(
-                'https://console.cloud.google.com/marketplace/product/bigquery-public-data/gbif-occurrences'
-              ),
-              derived: link('https://www.gbif.org/citation-guidelines#derivedDatasets'),
-            }}
-          />
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-10">
-          <OccurrenceSnapshotsTable
-            results={
-              data?.occurrenceSnapshots?.results
-                ? data?.occurrenceSnapshots?.results.filter(
-                    (r) => r?.request?.format === 'SIMPLE_PARQUET'
-                  )
-                : null
-            }
-          />
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-2">
-          <ArticleTitle>
-            <FormattedMessage id="occurrenceSnapshots.monthly.title" />
-          </ArticleTitle>
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-8">
-          <FormattedMessage id="occurrenceSnapshots.monthly.description" />
-        </ArticleTextContainer>
-        <ArticleTextContainer className="g-mb-2">
-          <OccurrenceSnapshotsTable results={data?.occurrenceSnapshots?.results} />
+        <ArticleTextContainer>
+          <div className="g-prose g-max-w-none dark:g-prose-invert">
+            <p>
+              <FormattedMessage id="occurrenceSnapshots.description" />
+            </p>
+            <h2>
+              <FormattedMessage id="occurrenceSnapshots.cloudDatasets.title" />
+            </h2>
+            <p>
+              <FormattedMessage
+                id="occurrenceSnapshots.cloudDatasets.description"
+                values={{
+                  azure: link('https://planetarycomputer.microsoft.com/dataset/gbif'),
+                  aws: link('https://registry.opendata.aws/gbif/'),
+                  gcs: link('https://console.cloud.google.com/storage/browser/public-datasets-gbif'),
+                  bq: link(
+                    'https://console.cloud.google.com/marketplace/product/bigquery-public-data/gbif-occurrences'
+                  ),
+                  derived: link('https://www.gbif.org/citation-guidelines#derivedDatasets'),
+                }}
+              />
+            </p>
+          </div>
+          <div className="g-my-8">
+            <OccurrenceSnapshotsTable
+              results={
+                data?.occurrenceSnapshots?.results
+                  ? data?.occurrenceSnapshots?.results.filter(
+                      (r) => r?.request?.format === 'SIMPLE_PARQUET'
+                    )
+                  : null
+              }
+            />
+          </div>
+          <div className="g-prose g-max-w-none dark:g-prose-invert">
+            <h2>
+              <FormattedMessage id="occurrenceSnapshots.monthly.title" />
+            </h2>
+            <p>
+              <FormattedMessage id="occurrenceSnapshots.monthly.description" />
+            </p>
+          </div>
+          <div className="g-my-8">
+            <OccurrenceSnapshotsTable results={data?.occurrenceSnapshots?.results} />
+          </div>
         </ArticleTextContainer>
       </PageContainer>
     </article>
