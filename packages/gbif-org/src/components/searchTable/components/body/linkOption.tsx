ef84a04d09f0480d1ed6fda01c6b1330036340ff
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2026-08-20 11:55:13 +0200

    fix: update links to be dynamic. close https://github.com/gbif/gbif-web/issues/1960

--- packages/gbif-org/src/components/searchTable/components/body/linkOption.tsx
+++ packages/gbif-org/src/components/searchTable/components/body/linkOption.tsx
@@ -3,14 +3,18 @@ import { MdLink } from 'react-icons/md';
 
 type Props = {
   to: string;
+  pageId: string;
+  variables?: Record<string, any>;
   children: React.ReactNode;
 };
 
-export function LinkOption({ to, children }: Props) {
+export function LinkOption({ to, pageId, variables, children }: Props) {
   return (
     <div>
       <DynamicLink
         to={to}
+        pageId={pageId}
+        variables={variables}
         className="g-pointer-events-auto hover:g-text-primary-500 hover:g-underline"
       >
         <span className="g-pe-1">
