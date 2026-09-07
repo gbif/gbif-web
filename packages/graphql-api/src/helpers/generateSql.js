0a18857d6018588fbd3fd759362b0691d0dcae9d
Dave Martin <djtfmartin@gmail.com>
2026-09-07 18:41:40 +0100

    feature(occurrence): use nested structs occurrence#543, occurrence#538

--- packages/graphql-api/src/helpers/generateSql.js
+++ packages/graphql-api/src/helpers/generateSql.js
@@ -11,25 +11,38 @@ function generateMachineDescription(parameters, sql) {
 }
 
 function nameLookup(name, checklistKey) {
-  if (checklistKey === config.gbifBackboneUUID) {
-    if (name === 'order') return `occurrence."order"`;
+
+  if (checklistKey === config.colUUID) {
+    if (name === 'order') {
+      return `occurrence."order"`;
+    }
     return `occurrence.${name}`;
+  }  
+  
+  if (checklistKey === config.gbifBackboneUUID) {
+    if (name === 'order') {
+      return `occurrence.gbif_classification."order"`;
+    }
+    return `occurrence.gbif_classification.${name}`;
   }
-  const lowername = name.toLowerCase();
-  const lookup = {};
-  // generate for convinence the name lookup for the taxonomic dimensions, e.g. kingdom, phylum, class, order, family, genus
-  const ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'taxon', 'acceptedtaxon'];
-  ranks.forEach((rank) => {
-    lookup[rank] = `occurrence.classificationdetails['${checklistKey}']['${rank}']`;
-    lookup[`${rank}key`] = `occurrence.classificationdetails['${checklistKey}']['${rank}key']`;
-  });
-  if (lowername === 'scientificname') {
-    return `occurrence.classificationdetails['${checklistKey}']['scientificname']`;
-  }
-  if (lowername === 'acceptedscientificname') {
-    return `occurrence.classificationdetails['${checklistKey}']['acceptedscientificname']`;
-  }
-  return lookup[lowername] || name;
+
+
+
+  // const lowername = name.toLowerCase();
+  // const lookup = {};
+  // // generate for convinence the name lookup for the taxonomic dimensions, e.g. kingdom, phylum, class, order, family, genus
+  // const ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'taxon', 'acceptedtaxon'];
+  // ranks.forEach((rank) => {
+  //   lookup[rank] = `occurrence.classificationdetails['${checklistKey}']['${rank}']`;
+  //   lookup[`${rank}key`] = `occurrence.classificationdetails['${checklistKey}']['${rank}key']`;
+  // });
+  // if (lowername === 'scientificname') {
+  //   return `occurrence.classificationdetails['${checklistKey}']['scientificname']`;
+  // }
+  // if (lowername === 'acceptedscientificname') {
+  //   return `occurrence.classificationdetails['${checklistKey}']['acceptedscientificname']`;
+  // }
+  // return lookup[lowername] || name;
 }
 
 export function getGbifMachineDescription(machineDescription, sql) {
